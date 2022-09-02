4a0a521338654159f35952826d2ed8c3e57c0872
kanitmann <kanitmann90@gmail.com>
2022-09-02 11:03:05 +0530

    Router setup completed

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,17 @@
+import { Link } from "react-router-dom";
 import Auth from 'components/auth'
 import Login from 'pages/login'
 
 export default function App() {
   return (
-    <Auth>
-      <Login />
-    </Auth>
+    <nav
+      style={{
+        borderBottom: "solid 1px",
+        paddingBottom: "1rem",
+      }}
+    >
+      <Link to="/login">Login</Link> |{" "}
+      <Link to="/home">Home</Link>
+    </nav>
   )
 }
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,11 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {
+  HashRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 import App from './App'
 import './styles/index.css'
 import 'antd/dist/antd.less'
+import Login from './pages/login'
+import Home from './pages/home'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="login" element={<Login />} />
+        <Route path="home" element={<Home />} />
+      </Routes>
+    </HashRouter>
   </React.StrictMode>,
 )
