bb869ddd1192168f7864d64df9ff94a4dfb069ea
Kanit Mann <kanitmann90@gmail.com>
2022-09-02 14:34:20 +0530

    Router setup completed (#3)
    This PR adds react-router-dom in the client to setup
    routes for frontend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,17 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Auth from 'components/auth'
 import Login from 'pages/login'
+import Home from 'pages/home'
 
 export default function App() {
   return (
     <Auth>
-      <Login />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </Auth>
   )
 }
