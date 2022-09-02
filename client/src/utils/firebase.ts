a158a869adcc3a5acde47f41ccb82e4e1483bc1b
Himanshu Nagrath <33781906+hnagrath09@users.noreply.github.com>
2022-09-02 10:16:38 +0530

    created env variables for firebase initialisation

--- client/src/utils/firebase.ts
+++ client/src/utils/firebase.ts
@@ -2,12 +2,12 @@ import { initializeApp } from 'firebase/app'
 import { getAuth, GoogleAuthProvider } from 'firebase/auth'
 
 const app = initializeApp({
-  apiKey: 'AIzaSyC4dwsorswISBdgIPedcNnJyRMY13WSlKg',
-  authDomain: 'daybreakhr-c855d.firebaseapp.com',
-  projectId: 'daybreakhr-c855d',
-  storageBucket: 'daybreakhr-c855d.appspot.com',
-  messagingSenderId: '597053514881',
-  appId: '1:597053514881:web:7f4d07d1c052defdb3d980',
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
 })
 
 export const auth = getAuth(app)
