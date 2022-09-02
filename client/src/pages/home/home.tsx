b7b402ad894b5e8772c8e96e20cb3c5481ade151
Himanshu Nagrath <33781906+hnagrath09@users.noreply.github.com>
2022-09-02 17:02:18 +0530

    styling changes in home page

--- client/src/pages/home/home.tsx
+++ client/src/pages/home/home.tsx
@@ -4,8 +4,8 @@ export default function Home() {
   const { user } = useAuth()
 
   return (
-    <div className="w-screen h-screen flex items-center justify-center">
-      <p>Welcome ${user?.displayName ?? user?.email}!!!</p>
+    <div className="w-screen h-screen flex flex-col items-center justify-center">
+      <p>Welcome {user?.displayName}!!!</p>
       <p>You are now authenticated with your gmail</p>
     </div>
   )
