6d84b2284d049f82da9d4e5ffca2fd60cf2acd7e
Himanshu Nagrath <33781906+hnagrath09@users.noreply.github.com>
2022-09-05 11:43:06 +0530

    Setup authentication using firebase in server (#7)
    This PR implements two new modules in the server:
    
    Firebase module: This module initialises firebase-admin
    at the onModuleInit lifecycle event. Service from firebase
    module will be used in auth module.
    
    Auth module: This module implements service to verify token
    and get user from firebase service using token and uid.
    Also, added middleware to get user from auth.service
    and put inside req header. A decorator to get user from
    req header for every http request.

---
+++ server/src/auth/auth.guard.ts
@@ -0,0 +1,13 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common'
+import { AuthService } from './auth.service'
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(private authService: AuthService) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request: Request = context.switchToHttp().getRequest()
+    // @ts-ignore
+    return !!request.user
+  }
+}
