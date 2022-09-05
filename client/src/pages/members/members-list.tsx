d1b82b7abe334e0f7242653430379d0a25421f5e
kanitmann <kanitmann90@gmail.com>
2022-09-05 23:35:27 +0530

    Created Member Table along with name search and role filter

---
+++ client/src/pages/members/members-list.tsx
@@ -0,0 +1,28 @@
+const memberTabs = [
+  {
+    key: '1',
+    name: 'Jimmy Brown',
+    role: 'admin',
+    actions: 'Sample Action',
+  },
+  {
+    key: '2',
+    name: 'Shawn Green',
+    role: 'admin',
+    actions: 'Sample Action',
+  },
+  {
+    key: '3',
+    name: 'Cobe Red',
+    role: 'user',
+    actions: 'Sample Action',
+  },
+  {
+    key: '4',
+    name: 'Jill Pink',
+    role: 'user',
+    actions: 'Sample Action',
+  },
+]
+
+export default memberTabs
