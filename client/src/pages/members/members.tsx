ea5b120d0c188d7f224f87b64439217efe233cbd
kanitmann <kanitmann90@gmail.com>
2022-09-06 09:48:31 +0530

    Implemented 'Member' Table

--- client/src/pages/members/members.tsx
+++ client/src/pages/members/members.tsx
@@ -1,114 +1,9 @@
-import { Key, useRef, useState } from 'react'
-import { Button, Input, InputRef, Space, Table } from 'antd'
-import Highlighter from 'react-highlight-words'
-import type { ColumnsType, TableProps } from 'antd/es/table'
-import type { ColumnType, FilterConfirmProps } from 'antd/es/table/interface'
-import { AiOutlineSearch } from 'react-icons/ai'
+import { Key } from 'react'
+import { Table } from 'antd'
+import type { ColumnsType } from 'antd/es/table'
 import memberTabs from './members-list'
 
 export default function Members() {
-  const [searchText, setSearchText] = useState('')
-  const [searchedColumn, setSearchedColumn] = useState('')
-  const searchInput = useRef<InputRef>(null)
-
-  type DataIndex = keyof DataType
-  const handleSearch = (
-    selectedKeys: string[],
-    confirm: (param?: FilterConfirmProps) => void,
-    dataIndex: DataIndex,
-  ) => {
-    confirm()
-    setSearchText(selectedKeys[0])
-    setSearchedColumn(dataIndex)
-  }
-
-  const handleReset = (clearFilters: () => void) => {
-    clearFilters()
-    setSearchText('')
-  }
-
-  // eslint-disable-next-line no-undef
-  const getColumnSearchProps = (
-    dataIndex: DataIndex,
-  ): ColumnType<DataType> => ({
-    filterDropdown: ({
-      setSelectedKeys,
-      selectedKeys,
-      confirm,
-      clearFilters,
-    }) => (
-      <div style={{ padding: 8 }}>
-        <Input
-          ref={searchInput}
-          placeholder={`Search ${dataIndex}`}
-          value={selectedKeys[0]}
-          onChange={(e) =>
-            setSelectedKeys(e.target.value ? [e.target.value] : [])
-          }
-          onPressEnter={() =>
-            handleSearch(selectedKeys as string[], confirm, dataIndex)
-          }
-          style={{ marginBottom: 8, display: 'block' }}
-        />
-        <Space>
-          <Button
-            type="primary"
-            onClick={() =>
-              handleSearch(selectedKeys as string[], confirm, dataIndex)
-            }
-            // icon={<SearchOutlined />}
-            size="small"
-            style={{ width: 90 }}
-          >
-            Search
-          </Button>
-          <Button
-            onClick={() => clearFilters && handleReset(clearFilters)}
-            size="small"
-            style={{ width: 90 }}
-          >
-            Reset
-          </Button>
-          <Button
-            type="link"
-            size="small"
-            onClick={() => {
-              confirm({ closeDropdown: false })
-              setSearchText((selectedKeys as string[])[0])
-              setSearchedColumn(dataIndex)
-            }}
-          >
-            Filter
-          </Button>
-        </Space>
-      </div>
-    ),
-    filterIcon: (filtered: boolean) => (
-      <AiOutlineSearch style={{ color: filtered ? '#1890ff' : undefined }} />
-    ),
-    onFilter: (value, record) =>
-      record[dataIndex]
-        .toString()
-        .toLowerCase()
-        .includes((value as string).toLowerCase()),
-    onFilterDropdownOpenChange: (visible: any) => {
-      if (visible) {
-        setTimeout(() => searchInput.current?.select(), 100)
-      }
-    },
-    render: (text: { toString: () => any }) =>
-      searchedColumn === dataIndex ? (
-        <Highlighter
-          highlightStyle={{ backgroundColor: '#ffc069', padding: 0 }}
-          searchWords={[searchText]}
-          autoEscape
-          textToHighlight={text ? text.toString() : ''}
-        />
-      ) : (
-        text
-      ),
-  })
-
   interface DataType {
     key: Key
     name: string
@@ -120,24 +15,10 @@ export default function Members() {
     {
       title: 'Name',
       dataIndex: 'name',
-      ...getColumnSearchProps('name'),
     },
     {
       title: 'Role',
       dataIndex: 'role',
-      filters: [
-        {
-          text: 'admin',
-          value: 'admin',
-        },
-        {
-          text: 'user',
-          value: 'user',
-        },
-      ],
-      filterSearch: true,
-      onFilter: (value: string, record) => record.role.startsWith(value),
-      width: '40%',
     },
     {
       title: 'Actions',
@@ -145,19 +26,14 @@ export default function Members() {
     },
   ]
 
-  const onChange: TableProps<DataType>['onChange'] = (
-    pagination,
-    filters,
-    sorter,
-    extra,
-  ) => {
-    console.log('params', pagination, filters, sorter, extra)
-  }
-
   return (
-    <div>
-      <Space />
-      <Table columns={columns} dataSource={memberTabs} onChange={onChange} />
+    <div className="mt-5">
+      <Table
+        columns={columns}
+        dataSource={memberTabs}
+        pagination={{ position: ['bottomCenter'] }}
+        bordered={true}
+      />
     </div>
   )
 }
