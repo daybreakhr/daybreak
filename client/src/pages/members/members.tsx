f4420c8a496034b397231f3a3cc76937b09df525
Himanshu Nagrath <33781906+hnagrath09@users.noreply.github.com>
2022-09-08 23:31:06 +0530

    completed add user modal in members page

--- client/src/pages/members/members.tsx
+++ client/src/pages/members/members.tsx
@@ -1,15 +1,17 @@
 import { useState } from 'react'
-import { Button, Form, Input, Modal, Select, Table } from 'antd'
 import { matchSorter } from 'match-sorter'
+import { Button, Input, Select, Table } from 'antd'
 import { AiOutlineFilter, AiOutlineSearch } from 'react-icons/ai'
 import useAuth from 'hooks/use-auth'
 import { sampleData, columns, Member } from './members-list'
+import AddUser from './components/add-user'
 
 export default function Members() {
   const { user } = useAuth()
+
   const [input, setInput] = useState('')
   const [filter, setFilter] = useState('')
-  const [isOpen, setIsOpen] = useState(false)
+  const [isVisible, setIsVisible] = useState(false)
 
   const data: Member[] = [
     {
@@ -53,62 +55,14 @@ export default function Members() {
 
         <div className="flex-1" />
 
-        <Button type="primary" onClick={() => setIsOpen(!isOpen)}>
-          Add Users
+        <Button type="primary" onClick={() => setIsVisible((prev) => !prev)}>
+          Add User
         </Button>
-        <Modal
-          okText="Add"
-          visible={isOpen}
-          title="Add new member"
-          onCancel={() => setIsOpen(false)}
-          okButtonProps={{ style: { display: 'none' } }}
-          cancelButtonProps={{ style: { display: 'none' } }}
-        >
-          <Form
-            name="basic"
-            autoComplete="off"
-            labelCol={{ span: 8 }}
-            wrapperCol={{ span: 16 }}
-            initialValues={{ remember: true }}
-            onFinish={(values: any) => {
-              console.log(values)
-              setIsOpen(false)
-            }}
-            onFinishFailed={(errorInfo: any) => console.log(errorInfo)}
-          >
-            <Form.Item
-              label="Email"
-              name="email"
-              rules={[
-                {
-                  type: 'email',
-                  required: true,
-                  message: 'Please input a valid email!',
-                },
-              ]}
-            >
-              <Input />
-            </Form.Item>
-            <Form.Item
-              label="Role"
-              name="role"
-              rules={[{ required: true, message: 'Please select the role!' }]}
-            >
-              <Select>
-                <Select.Option value="admin">Admin</Select.Option>
-                <Select.Option value="member">Member</Select.Option>
-              </Select>
-            </Form.Item>
-            <Form.Item wrapperCol={{ offset: 16, span: 16 }}>
-              <Button type="primary" htmlType="submit">
-                Add
-              </Button>
-            </Form.Item>
-          </Form>
-        </Modal>
       </div>
 
       <Table dataSource={filterRoleData} columns={columns} />
+
+      <AddUser isVisible={isVisible} onClose={() => setIsVisible(false)} />
     </div>
   )
 }
