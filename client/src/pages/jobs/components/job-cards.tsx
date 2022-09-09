d3b4c7092ff7f4016d1be8e559a344a1e972e10c
kanitmann <kanitmann90@gmail.com>
2022-09-09 20:51:43 +0530

    UI Update, added icons to info cards

--- client/src/pages/jobs/components/job-cards.tsx
+++ client/src/pages/jobs/components/job-cards.tsx
@@ -1,73 +1,99 @@
 import { Card, Col, Row } from 'antd'
+import {
+  AiOutlineCarryOut,
+  AiOutlineExclamationCircle,
+  AiOutlineInfoCircle,
+  AiOutlineSolution,
+} from 'react-icons/ai'
 
 export default function jobCards() {
   return (
-    <div className="m-8 p-4">
+    <div className="p-4 m-8">
       <Row gutter={12}>
         <Col span={6}>
-          <div className="bg-white rounded-md shadow-md pl-4">
+          <div className="pl-4 bg-white rounded-md shadow-md">
             <Card
               title="Active Jobs"
               headStyle={{
-                color: 'rgba(0, 0, 0, 0.5)',
+                color: 'rgba(0, 0, 0, 0.7',
                 border: 0,
               }}
               bordered={false}
             >
-              <div className="text-gray-500 -mt-6 flex flex-col">
-                <span className="text-2xl font-medium">5</span>
-                <span className="text-sm">Total Jobs</span>
+              <div className="flex">
+                <div className="flex flex-col -mt-6 text-gray-600">
+                  <span className="text-2xl font-medium">2</span>
+                  <span className="text-sm">Total Jobs</span>
+                </div>
+                <div className="flex-1 -mt-10">
+                  <AiOutlineInfoCircle className="float-right p-1 text-4xl text-blue-700 bg-blue-100 rounded-md" />
+                </div>
               </div>
             </Card>
           </div>
         </Col>
         <Col span={6}>
-          <div className="bg-white rounded-md shadow-md pl-4">
+          <div className="pl-4 bg-white rounded-md shadow-md">
             <Card
               title="Jobs With Process Today"
               headStyle={{
-                color: 'rgba(0, 0, 0, 0.5)',
+                color: 'rgba(0, 0, 0, 0.7)',
                 border: 0,
               }}
               bordered={false}
             >
-              <div className="text-gray-500 -mt-6 flex flex-col">
-                <span className="text-2xl font-medium">3</span>
-                <span className="text-sm">Last week analytics</span>
+              <div className="flex">
+                <div className="flex flex-col -mt-6 text-gray-600">
+                  <span className="text-2xl font-medium">0</span>
+                  <span className="text-sm">Last week analytics</span>
+                </div>
+                <div className="flex-1 -mt-10">
+                  <AiOutlineCarryOut className="float-right p-1 text-4xl text-green-700 bg-green-100 rounded-md" />
+                </div>
               </div>
             </Card>
           </div>
         </Col>
         <Col span={6}>
-          <div className="bg-white rounded-md shadow-md pl-4">
+          <div className="pl-4 bg-white rounded-md shadow-md">
             <Card
               title="Jobs Assigned To Me"
               headStyle={{
-                color: 'rgba(0, 0, 0, 0.5)',
+                color: 'rgba(0, 0, 0, 0.7)',
                 border: 0,
               }}
               bordered={false}
             >
-              <div className="text-gray-500 -mt-6 flex flex-col">
-                <span className="text-2xl font-medium">2</span>
-                <span className="text-sm">Last week analytics</span>
+              <div className="flex">
+                <div className="flex flex-col -mt-6 text-gray-600">
+                  <span className="text-2xl font-medium">2</span>
+                  <span className="text-sm">Last week analytics</span>
+                </div>
+                <div className="flex-1 -mt-10">
+                  <AiOutlineSolution className="float-right p-1 text-4xl text-orange-700 bg-orange-100 rounded-md" />
+                </div>
               </div>
             </Card>
           </div>
         </Col>
         <Col span={6}>
-          <div className="bg-white rounded-md shadow-md pl-4">
+          <div className="pl-4 bg-white rounded-md shadow-md">
             <Card
               title="High Priority Jobs"
               headStyle={{
-                color: 'rgba(0, 0, 0, 0.5)',
+                color: 'rgba(0, 0, 0, 0.7)',
                 border: 0,
               }}
               bordered={false}
             >
-              <div className="text-gray-500 -mt-6 flex flex-col">
-                <span className="text-2xl font-medium">1</span>
-                <span className="text-sm">Last week analytics</span>
+              <div className="flex">
+                <div className="flex flex-col -mt-6 text-gray-600">
+                  <span className="text-2xl font-medium">1</span>
+                  <span className="text-sm">Last week analytics</span>
+                </div>
+                <div className="flex-1 -mt-10">
+                  <AiOutlineExclamationCircle className="float-right p-1 text-4xl text-red-700 bg-red-100 rounded-md" />
+                </div>
               </div>
             </Card>
           </div>
