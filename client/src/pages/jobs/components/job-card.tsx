670b03db781b13b734c6782dd15263fd2596834c
Himanshu Nagrath <33781906+hnagrath09@users.noreply.github.com>
2022-09-10 15:07:22 +0530

    Added role in user context from firebase custom claims

--- client/src/pages/jobs/components/job-card.tsx
+++ client/src/pages/jobs/components/job-card.tsx
@@ -17,9 +17,9 @@ export default function JobCard({
   return (
     <div className="shadow-md bg-white py-4 flex items-start justify-between px-6 rounded-md">
       <div className="flex flex-col">
-        <p className="text-base font-medium mb-6">{title}</p>
-        <span className="text-2xl font-semibold mb-2">{count}</span>
-        <span>{description}</span>
+        <p className="text-base font-medium mb-4 text-gray-700">{title}</p>
+        <span className="text-3xl font-semibold mb-0.5">{count}</span>
+        <span className="text-gray-500 font-light">{description}</span>
       </div>
       {cloneElement(icon, {
         className: clsx('text-4xl rounded-md p-2', icon.props.className),
