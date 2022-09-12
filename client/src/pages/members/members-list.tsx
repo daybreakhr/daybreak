9272162ff7210a8fd222b719b98f7fe6401d7180
kanitmann <kanitmann90@gmail.com>
2022-09-12 14:47:35 +0530

    Replaced 'edit' column with dropbox with role actions

--- client/src/pages/members/members-list.tsx
+++ client/src/pages/members/members-list.tsx
@@ -1,7 +1,33 @@
-import { Avatar, Typography } from 'antd'
+import { Avatar, Dropdown, Menu, MenuProps, message } from 'antd'
 import { capitalize } from 'lodash'
 import type { ColumnsType } from 'antd/es/table'
 import { Member } from 'types/member'
+import { AiOutlineMore } from 'react-icons/ai'
+
+const onClick: MenuProps['onClick'] = ({ key }) => {
+  message.info(`Click on item ${key}`)
+}
+
+const menu = (props: any) => {
+  return (
+    <Menu>
+      {props === 'Admin' ? (
+        <Menu>
+          <Menu.Item key="1" onClick={onClick}>
+            Change Role
+          </Menu.Item>
+          <Menu.Item key="2" onClick={onClick} style={{ color: '#ff000f' }}>
+            Remove User
+          </Menu.Item>
+        </Menu>
+      ) : (
+        <Menu.Item key="1" onClick={onClick} style={{ color: '#ff000f' }}>
+          Leave Workspace
+        </Menu.Item>
+      )}
+    </Menu>
+  )
+}
 
 export const columns: ColumnsType<Member> = [
   {
@@ -24,5 +50,15 @@ export const columns: ColumnsType<Member> = [
     key: 'role',
     render: (role: string) => capitalize(role),
   },
-  { title: '', render: () => <Typography.Link>Edit</Typography.Link> },
+  {
+    title: '',
+    dataIndex: 'role',
+    render: (role: string) => (
+      <>
+        <Dropdown trigger={['click']} overlay={menu(role)}>
+          <AiOutlineMore className="cursor-pointer" />
+        </Dropdown>
+      </>
+    ),
+  },
 ]
