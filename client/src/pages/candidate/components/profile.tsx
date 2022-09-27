82873b94fab4a78e2d8e6dbc48b64b76d14b0ab5
kanitmann <kanitmann90@gmail.com>
2022-09-27 12:41:30 +0530

    Conversations Resolved(3/3)

--- client/src/pages/candidate/components/profile.tsx
+++ client/src/pages/candidate/components/profile.tsx
@@ -1,31 +1,36 @@
-import { Divider, Image, Typography } from 'antd'
+import { Image, Typography } from 'antd'
 import { AiOutlineCloudDownload } from 'react-icons/ai'
+import { sampleData } from './profile-list'
 
 export default function Profile() {
   return (
-    <div className="p-4 text-[#2c2c2c]">
-      <p className="text-lg font-semibold ">Personal Details</p>
+    <div className="p-4 text-gray-800">
+      <p className="text-lg font-semibold">Personal Details</p>
       <div className="grid grid-cols-3 gap-5 mb-6">
         <div className="col-span-2 ">
           <div className="grid grid-cols-2 gap-5">
             <div className="ml-4">
               <div>First Name</div>
-              <div className="text-base font-medium">Kanit</div>
+              <div className="text-base font-medium">
+                {sampleData[0].firstname}
+              </div>
             </div>
             <div>
               <div>Last Name</div>
-              <div className="text-base font-medium">Mann</div>
+              <div className="text-base font-medium">
+                {sampleData[0].lastname}
+              </div>
             </div>
 
             <div className="ml-4">
               <div>Gender</div>
-              <div className="text-base font-medium">Male</div>
+              <div className="text-base font-medium">
+                {sampleData[0].gender}
+              </div>
             </div>
             <div>
               <div>Date of Birth</div>
-              <div className="text-base font-medium">
-                July 07, 1995 (27 yrs old)
-              </div>
+              <div className="text-base font-medium">{sampleData[0].dob}</div>
             </div>
           </div>
         </div>
@@ -40,58 +45,52 @@ export default function Profile() {
         </div>
       </div>
 
-      <Divider />
+      <hr />
 
-      <p className="text-lg font-semibold">Cover Letter</p>
-
+      <p className="text-lg font-semibold mt-4">Cover Letter</p>
       <Typography.Paragraph
+        className="pb-2"
         editable
         ellipsis={{ rows: 3, expandable: true, symbol: 'Read More' }}
       >
-        Having recently finished a 2-year contract in software development after
-        completing my Bachelor of Science in Computer Science, I am ready to
-        start the next chapter in my life. So, I was thrilled when I came across
-        your job post in search of IT candidates in software engineering. With
-        both my educational and professional background in the entire software
-        dev life cycle, I believe I have what it takes to be the perfect choice
-        for Cyber Science Tech. As an IT specialist focusing on delivering top
-        results in systems and software development, I know I have also worked
-        on xys dklsfjafj sample test text.
+        {sampleData[0].coverletter}
       </Typography.Paragraph>
 
-      <Divider />
+      <hr />
 
-      <p className="text-lg font-semibold">Experience</p>
+      <p className="text-lg font-semibold mt-4">Experience</p>
       <div className="ml-10">
         <ul className="list-disc">
           <li className="mb-5">
             <div className="text-base font-normal">
-              Front End Developer at Infosys
+              {sampleData[0].experience}
             </div>
             <div className="text-sm text-gray-500">
-              April 2019 - June 2021 ( 2 Years 3 Months )
+              {sampleData[0].experienceduration}
             </div>
           </li>
           <li className="mb-5">
             <div className="text-base font-normal">
-              Front End Developer at Wipro
+              {sampleData[0].experience}
             </div>
             <div className="text-sm text-gray-500">
-              April 2016 - June 2018 ( 2 Years 3 Months )
+              {sampleData[0].experienceduration}
             </div>
           </li>
         </ul>
       </div>
 
-      <Divider />
-      <p className="text-lg font-semibold">Education</p>
+      <hr />
+      <p className="text-lg font-semibold mt-4">Education</p>
       <div className="ml-10">
         <ul className="list-disc">
           <li className="mb-5">
             <div className="text-base font-normal">
-              Sample Institute of Technology, Sample City
+              {sampleData[0].education}
             </div>
-            <div className="text-sm text-gray-500">2012 - 2016 </div>
+            <div className="text-sm text-gray-500">
+              {sampleData[0].educationduration}{' '}
+            </div>
           </li>
         </ul>
       </div>
