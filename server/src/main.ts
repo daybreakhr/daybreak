e313fca1242ae90c228af3da12f15efda90f18ed
Himanshu Nagrath <33781906+hnagrath09@users.noreply.github.com>
2022-10-04 14:37:32 +0530

    Fix CORS issue in server

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,7 +5,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule)
 
   app.enableCors({
-    origin: '*',
+    origin:
+      process.env.NODE_ENV === 'production'
+        ? 'https://daybreakhr-c855d*.web.app'
+        : '*',
   })
 
   await app.listen(process.env.PORT ?? 8000)
