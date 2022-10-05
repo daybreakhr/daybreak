9e745925d552e9f57e9912eff4cfadc4bab90021
Himanshu Nagrath <33781906+hnagrath09@users.noreply.github.com>
2022-10-05 11:10:52 +0530

    Removed origin from cors config to fix cors error

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,12 +4,7 @@ import { AppModule } from './app.module'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
 
-  app.enableCors({
-    origin:
-      process.env.NODE_ENV === 'production'
-        ? 'https://daybreakhr-c855d*.web.app'
-        : '*',
-  })
+  app.enableCors()
 
   await app.listen(process.env.PORT ?? 8000)
 }
