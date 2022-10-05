ab197f1e9af30b962869f97a92a40ba6db64b0bb
Himanshu Nagrath <33781906+hnagrath09@users.noreply.github.com>
2022-10-05 11:29:25 +0530

    Added prod url for cors origin

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,7 +4,9 @@ import { AppModule } from './app.module'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
 
-  app.enableCors()
+  app.enableCors({
+    origin: process.env.NODE_ENV ? 'https://daybreakhr-c855d.web.app' : '*',
+  })
 
   await app.listen(process.env.PORT ?? 8000)
 }
