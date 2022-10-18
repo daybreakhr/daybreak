1545798d831247c9fe526fe3fbb75a59de40c01f
Himanshu Nagrath <33781906+hnagrath09@users.noreply.github.com>
2022-10-18 21:56:40 +0530

    added feedback form to add new feedback

--- client/src/pages/candidate/components/feedback.tsx
+++ client/src/pages/candidate/components/feedback.tsx
@@ -1,13 +1,21 @@
-import { Avatar, Button } from 'antd'
+import { useState } from 'react'
+import { Avatar, Button, Rate } from 'antd'
 import { AiOutlinePlus } from 'react-icons/ai'
+import FeedbackForm from './feedback-form'
 import { notes } from '../constants/feedback'
 
 export default function Feedback() {
+  const [feedbackModal, setFeedbackModal] = useState(false)
+
   return (
-    <div className="p-4 mb-4 text-gray-800 bg-white shadow-md rounded-b-md">
+    <div className="p-4 text-gray-800 bg-white shadow-md rounded-b-md">
       <div className="flex items-center justify-between mb-6">
         <p className="text-lg font-semibold">Interview Feedback</p>
-        <Button type="primary" icon={<AiOutlinePlus />}>
+        <Button
+          type="primary"
+          icon={<AiOutlinePlus />}
+          onClick={() => setFeedbackModal(true)}
+        >
           Add Feedback
         </Button>
       </div>
@@ -24,18 +32,24 @@ export default function Feedback() {
 
                 <div className="flex items-center justify-between mb-1">
                   <p className="font-semibold">{title}</p>
+
                   <p>
-                    <span className="font-medium">Score:</span> {score}/10
+                    <span className="font-medium mr-2">Score</span>
+                    <Rate disabled allowHalf defaultValue={score} />
                   </p>
                 </div>
 
-                <p className="mb-2">{comment}</p>
+                <p className="mb-2 whitespace-pre-line">{comment}</p>
                 <p className="text-xs text-gray-500">{createdAt}</p>
               </div>
             </div>
           ),
         )}
       </div>
+      <FeedbackForm
+        visible={feedbackModal}
+        onCancel={() => setFeedbackModal(false)}
+      />
     </div>
   )
 }
--- client/src/pages/candidate/constants/feedback.ts
+++ client/src/pages/candidate/constants/feedback.ts
@@ -4,7 +4,7 @@ export const notes = [
     title: 'Telephone Screening',
     comment:
       'By default, Tailwind removes all of the default browser styling from paragraphs, headings, lists and more. This ends up being really useful for building application UIs because you spend less time undoing user-agent styles, but when you really are just trying to style some content that came from a rich-text editor in a CMS or a markdown file, it can be surprising and unintuitive.',
-    score: 9,
+    score: 4.5,
     createdAt: '1 week ago',
     photoURL: 'https://randomuser.me/api/portraits/men/1.jpg',
     name: 'Mitchell Holland',
@@ -14,7 +14,7 @@ export const notes = [
     title: 'Coding Interview - HTML/CSS/JS',
     comment:
       'The @tailwindcss/typography plugin is our attempt to give you what you actually want, without any of the downsides of doing something stupid like disabling our base styles.\nIt adds a new prose class that you can slap on any block of vanilla HTML content and turn it into a beautiful, well-formatted document:',
-    score: 8,
+    score: 4,
     createdAt: '4 days ago',
     photoURL: 'https://randomuser.me/api/portraits/men/41.jpg',
     name: 'Brennan Lowe',
@@ -24,7 +24,7 @@ export const notes = [
     title: 'Coding Interview - Bug Bash',
     comment:
       "We hear you, but we're not convinced that simply disabling our base styles is what you really want. You don't want to have to remove annoying margins every time you use a p element in a piece of your dashboard UI. And I doubt you really want your blog posts to use the user-agent styles either — you want them to look awesome, not awful.",
-    score: 7.5,
+    score: 3.5,
     createdAt: '2 days ago',
     photoURL: 'https://randomuser.me/api/portraits/women/96.jpg',
     name: 'Deanna Neal',
