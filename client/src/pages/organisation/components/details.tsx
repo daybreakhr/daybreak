59778d2f9c3c6efbe767dd0328127ec5c87ee37c
Himanshu Nagrath <33781906+hnagrath09@users.noreply.github.com>
2022-10-22 15:25:20 +0530

    added uploader in organisation settings to upload logo

--- client/src/pages/organisation/components/details.tsx
+++ client/src/pages/organisation/components/details.tsx
@@ -1,13 +1,25 @@
-import { Button, Form, Input, message } from 'antd'
+import { useState } from 'react'
+import { AiOutlinePlus } from 'react-icons/ai'
+import type { UploadFile, UploadProps } from 'antd'
+import { Button, Form, Input, message, Upload } from 'antd'
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
+import useAuth from 'hooks/use-auth'
 import { fetchOrganisation, updateOrganisation } from '../queries'
+import { customRequest } from '../utils'
 
 export default function OrgDetails() {
+  const { user } = useAuth()
   const [form] = Form.useForm()
+  const [fileList, setFileList] = useState<UploadFile[]>([])
 
   useQuery(['organisation'], fetchOrganisation, {
     onSuccess: (data) => {
-      form.setFieldsValue(data)
+      form.setFieldsValue({ ...data })
+      if (data.logo) {
+        setFileList([
+          { uid: data.logo, name: data.name, status: 'done', url: data.logo },
+        ])
+      }
     },
   })
 
@@ -23,9 +35,27 @@ export default function OrgDetails() {
     },
   )
 
+  const handleChange: UploadProps['onChange'] = ({ fileList: newFileList }) => {
+    setFileList(newFileList.slice(-1))
+  }
+
   return (
     <div className="p-4 m-8 bg-white rounded-md shadow-md">
       <p className="mb-4 font-sans text-xl font-medium">Organisation Details</p>
+      <Upload
+        fileList={fileList}
+        listType="picture-card"
+        onChange={handleChange}
+        customRequest={customRequest(user)}
+        action={`workspace/${import.meta.env.VITE_WORKSPACE_ID}/upload`}
+        showUploadList={{ showPreviewIcon: false, showRemoveIcon: false }}
+      >
+        <div className="flex flex-col items-center justify-center w-24 h-24 space-y-3 rounded">
+          <AiOutlinePlus />
+          <p>Upload</p>
+        </div>
+      </Upload>
+
       <Form
         form={form}
         layout="vertical"
