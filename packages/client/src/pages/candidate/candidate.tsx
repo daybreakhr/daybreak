d41948ebeb1ab1a8fdc9af8648556a604800c34b
Himanshu Nagrath <33781906+hnagrath09@users.noreply.github.com>
2022-10-25 22:47:20 +0530

    Feedback button in profile tab should change tab

--- packages/client/src/pages/candidate/candidate.tsx
+++ packages/client/src/pages/candidate/candidate.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { Tabs } from 'antd'
 import { Link } from 'react-router-dom'
 import { AiOutlineArrowLeft } from 'react-icons/ai'
@@ -6,6 +7,8 @@ import Details from './components/details'
 import Feedback from './components/feedback'
 
 export default function Candidate() {
+  const [activeKey, setActiveKey] = useState('profile')
+
   return (
     <div className="flex flex-col flex-1 px-8 pt-4 pb-8">
       <Link to="/candidates" className="flex items-center mb-4 space-x-2">
@@ -15,9 +18,12 @@ export default function Candidate() {
 
       <div className="flex flex-1 space-x-4">
         <div className="flex-1 overflow-hidden rounded-md">
-          <Tabs>
+          <Tabs
+            activeKey={activeKey}
+            onChange={(newKey) => setActiveKey(newKey)}
+          >
             <Tabs.TabPane tab="Candidate Profile" key="profile">
-              <Profile />
+              <Profile onChange={() => setActiveKey('feedback')} />
             </Tabs.TabPane>
             <Tabs.TabPane tab="Feedback" key="feedback">
               <Feedback />
