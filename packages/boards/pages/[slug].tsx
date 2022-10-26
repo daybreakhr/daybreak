df8f7701adab2e1d7f8b6d4ad81c1346d732f06e
Himanshu Nagrath <33781906+hnagrath09@users.noreply.github.com>
2022-10-26 20:59:17 +0530

    Added meta tags in boards app to show workspace name

--- packages/boards/pages/[slug].tsx
+++ packages/boards/pages/[slug].tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 import Image from 'next/image'
 import type { Job, Workspace } from '@prisma/client'
 import type { GetStaticPaths, GetStaticProps } from 'next'
@@ -38,26 +39,39 @@ type WorkspaceHomeProps = {
 
 export default function WorkspaceHome({ workspace }: WorkspaceHomeProps) {
   return (
-    <div className="w-screen h-screen max-w-3xl py-8 mx-auto">
-      <Image
-        width={48}
-        height={48}
-        alt="Company logo"
-        src={workspace.logo ?? ''}
-      />
-      <div className="mb-4 prose prose-stone">
-        <h3 className="text-lg font-semibold">{workspace.name}</h3>
-        <p className="text-sm text-justify">{workspace.description}</p>
-        <h4>Current Job Openings</h4>
-      </div>
+    <>
+      <Head>
+        <title>Jobs at {workspace.name}</title>
+        <link rel="icon" type="image/svg+xml" href={workspace.logo ?? ''} />
+        <meta property="og:title" content={workspace.name} key="title" />
+        <meta
+          property="og:description"
+          content={workspace.description ?? ''}
+          key="description"
+        />
+      </Head>
 
-      {workspace.Job.map((job) =>
-        job.isPublished ? (
-          <div className="mb-4" key={job.id}>
-            <p className="text-indigo-600 hover:underline">{job.title}</p>
-          </div>
-        ) : null,
-      )}
-    </div>
+      <div className="w-screen h-screen max-w-3xl py-8 mx-auto">
+        <Image
+          width={48}
+          height={48}
+          alt="Company logo"
+          src={workspace.logo ?? ''}
+        />
+        <div className="mb-4 prose prose-stone">
+          <h3 className="text-lg font-semibold">{workspace.name}</h3>
+          <p className="text-sm text-justify">{workspace.description}</p>
+          <h4>Current Job Openings</h4>
+        </div>
+
+        {workspace.Job.map((job) =>
+          job.isPublished ? (
+            <div className="mb-4" key={job.id}>
+              <p className="text-indigo-600 hover:underline">{job.title}</p>
+            </div>
+          ) : null,
+        )}
+      </div>
+    </>
   )
 }
