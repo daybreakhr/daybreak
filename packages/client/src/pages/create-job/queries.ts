0e5cf4496acf3c151d865c87088a70afbc578858
Himanshu Nagrath <33781906+hnagrath09@users.noreply.github.com>
2022-11-05 10:58:36 +0530

    Fix api url to fetch job by id in create job page

--- packages/client/src/pages/create-job/queries.ts
+++ packages/client/src/pages/create-job/queries.ts
@@ -5,7 +5,7 @@ import { Job } from 'types/job'
 const WORKSPACE_ID = '6317158147089f094cd4598e'
 
 export async function fetchJobById(jobId: string) {
-  const { data } = await client.get<Job>(`${WORKSPACE_ID}/jobs/${jobId}`)
+  const { data } = await client.get<Job>(`jobs/${jobId}`)
   return data
 }
 
