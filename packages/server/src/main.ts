09546298afbf19353d221af3e80780187b51cf86
Himanshu Nagrath <33781906+hnagrath09@users.noreply.github.com>
2022-11-07 22:20:41 +0530

    added cors for boards app

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule)
 
   app.enableCors({
-    origin: process.env.FRONTEND_URL,
+    origin: [process.env.FRONTEND_URL, process.env.BOARDS_URL],
   })
 
   await app.listen(process.env.PORT ?? 8000)
