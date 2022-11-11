e1218ffae0a6acdc52c0ab05554b076691a46ba8
hnagrath09 <him.nagrath@gmail.com>
2022-11-11 16:27:10 +0530

    Fix horizontal scrolls in appshell

--- packages/client/src/components/app-shell/app-shell.tsx
+++ packages/client/src/components/app-shell/app-shell.tsx
@@ -4,10 +4,10 @@ import Header from 'components/header'
 
 export default function AppShell() {
   return (
-    <div className="flex flex-col w-screen h-screen">
+    <div className="flex flex-col w-screen h-screen overflow-hidden">
       <Header />
 
-      <div className="flex flex-1">
+      <div className="flex flex-1 overflow-hidden">
         <Sidebar />
         <div className="flex flex-col flex-1 overflow-y-auto bg-gray-100">
           <Outlet />
