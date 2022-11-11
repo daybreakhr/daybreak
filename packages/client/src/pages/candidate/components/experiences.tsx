08212bb07095a0730d8f387ebfb8dc365c412c35
Himanshu Nagrath <33781906+hnagrath09@users.noreply.github.com>
2022-11-11 14:39:46 +0530

    refactoring in experiences widget

--- packages/client/src/pages/candidate/components/experiences.tsx
+++ packages/client/src/pages/candidate/components/experiences.tsx
@@ -2,7 +2,7 @@ import dayjs from 'dayjs'
 import { Avatar } from 'antd'
 import { range } from 'lodash'
 import type { ResumeDataWorkExperienceItem } from '@affinda/affinda'
-import { Switch } from 'ui-kit'
+import { Show, Switch } from 'ui-kit'
 
 type ExperiencesProps = {
   experiences: ResumeDataWorkExperienceItem[] | undefined
@@ -14,51 +14,68 @@ export default function Experiences({
   isLoading,
 }: ExperiencesProps) {
   return (
-    <div className="p-4 text-gray-800 bg-white rounded-md shadow-md">
-      <p className="mb-4 text-lg font-semibold">Experiences</p>
-      <ul className="space-y-4">
-        <Switch>
-          <Switch.Match when={isLoading}>
-            {range(2).map((val) => (
-              <li key={val}>
-                <div className="flex items-start space-x-3">
-                  <div className="w-8 h-8 bg-gray-100 rounded-full animate-pulse" />
+    <div className="text-gray-800 bg-white rounded-md shadow-md">
+      <p className="m-4 text-lg font-semibold">Experiences</p>
 
-                  <div className="space-y-2">
-                    <div className="w-20 h-6 bg-gray-100 rounded animate-pulse" />
-                    <div className="w-48 h-6 bg-gray-100 rounded animate-pulse" />
-                  </div>
-
-                  <div className="flex-1" />
-
-                  <div className="w-24 h-6 bg-gray-100 rounded animate-pulse" />
-                </div>
-              </li>
-            ))}
-          </Switch.Match>
-
-          <Switch.Match when={experiences}>
-            {(data) =>
-              data.map(({ jobTitle, organization, id, dates }) => (
-                <li key={id}>
+      <div className="p-4 overflow-y-auto max-h-64">
+        <ul className="space-y-4">
+          <Switch>
+            <Switch.Match when={isLoading}>
+              {range(2).map((val) => (
+                <li key={val}>
                   <div className="flex items-start space-x-3">
-                    <Avatar>{organization?.[0]}</Avatar>
-                    <div>
-                      <p className="font-medium">{organization}</p>
-                      <p className="text-sm text-gray-800">{jobTitle}</p>
+                    <div className="w-8 h-8 bg-gray-100 rounded-full animate-pulse" />
+
+                    <div className="space-y-2">
+                      <div className="w-20 h-6 bg-gray-100 rounded animate-pulse" />
+                      <div className="w-48 h-6 bg-gray-100 rounded animate-pulse" />
                     </div>
+
                     <div className="flex-1" />
-                    <p className="text-gray-500">
-                      {dayjs(dates?.startDate).format('MMM YY')} -{' '}
-                      {dayjs(dates?.endDate).format('MMM YY')}
-                    </p>
+
+                    <div className="w-24 h-6 bg-gray-100 rounded animate-pulse" />
                   </div>
                 </li>
-              ))
-            }
-          </Switch.Match>
-        </Switch>
-      </ul>
+              ))}
+            </Switch.Match>
+
+            <Switch.Match when={experiences}>
+              {(data) =>
+                data.map(({ jobTitle, organization, id, dates }) => (
+                  <li key={id}>
+                    <div className="flex items-start space-x-3">
+                      <Avatar>{organization?.[0]}</Avatar>
+                      <div>
+                        <p className="font-medium">{organization}</p>
+                        <p className="text-sm text-gray-800">{jobTitle}</p>
+                      </div>
+                      <div className="flex-1" />
+
+                      <p className="text-gray-500">
+                        <Show when={dates?.startDate}>
+                          {(date) => (
+                            <span>{dayjs(date).format('MMM YY')} - </span>
+                          )}
+                        </Show>
+
+                        <Show when={dates?.endDate}>
+                          {(date) => (
+                            <span>
+                              {dates?.isCurrent
+                                ? 'Present'
+                                : dayjs(date).format('MMM YY')}
+                            </span>
+                          )}
+                        </Show>
+                      </p>
+                    </div>
+                  </li>
+                ))
+              }
+            </Switch.Match>
+          </Switch>
+        </ul>
+      </div>
     </div>
   )
 }
