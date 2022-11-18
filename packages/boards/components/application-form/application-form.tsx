83ed0fd463e2e1ce9cfd4f986c0cd2aa3e70109e
kanitmann <kanitmann90@gmail.com>
2022-11-18 17:43:13 +0530

    Added URL check

--- packages/boards/components/application-form/application-form.tsx
+++ packages/boards/components/application-form/application-form.tsx
@@ -139,7 +139,10 @@ export default function ApplicationForm({ workspaceId }: ApplicationFormProps) {
       <Form.Item
         name="linkedInUrl"
         label="Linkedin Profile URL"
-        rules={[{ required: true, message: 'Please input your LinkedIn URL' }]}
+        rules={[
+          { type: 'url', message: 'The input is not a valid URL!' },
+          { required: true, message: 'Please input your LinkedIn URL' },
+        ]}
       >
         <Input placeholder="https://linkedin.com/in/username" />
       </Form.Item>
