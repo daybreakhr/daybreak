3c431c48533addd58a9f2495a96378d9d4ce53ca
Himanshu Nagrath <33781906+hnagrath09@users.noreply.github.com>
2022-11-29 21:56:46 +0530

    Updated ant design and theme in client and boards (#52)
    This PR updates antd in client and boards app. We are
    now using ConfigProvider to set primary color and theme
    instead of less configurations

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,3 +1,4 @@
+import { ConfigProvider } from 'antd'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
@@ -24,25 +25,30 @@ const queryClient = new QueryClient({
 export default function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <Auth>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Login />} />
-            <Route path="/onboarding" element={<Onboarding />} />
-            <Route path="/" element={<AuthLayout component={AppShell} />}>
-              <Route path="home" element={<Home />} />
-              <Route path="jobs" element={<Jobs />} />
-              <Route path="jobs/:jobId" element={<Job />} />
-              <Route path="jobs/:jobId/create" element={<CreateJob />} />
-              <Route path="jobs/:jobId/publish" element={<PublishJob />} />
-              <Route path="candidates" element={<Candidates />} />
-              <Route path="candidates/:candidateId" element={<Candidate />} />
-              <Route path="settings/organisation" element={<Organisation />} />
-              <Route path="settings/members" element={<Members />} />
-            </Route>
-          </Routes>
-        </BrowserRouter>
-      </Auth>
+      <ConfigProvider theme={{ token: { colorPrimary: '#9155fd' } }}>
+        <Auth>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Login />} />
+              <Route path="/onboarding" element={<Onboarding />} />
+              <Route path="/" element={<AuthLayout component={AppShell} />}>
+                <Route path="home" element={<Home />} />
+                <Route path="jobs" element={<Jobs />} />
+                <Route path="jobs/:jobId" element={<Job />} />
+                <Route path="jobs/:jobId/create" element={<CreateJob />} />
+                <Route path="jobs/:jobId/publish" element={<PublishJob />} />
+                <Route path="candidates" element={<Candidates />} />
+                <Route path="candidates/:candidateId" element={<Candidate />} />
+                <Route
+                  path="settings/organisation"
+                  element={<Organisation />}
+                />
+                <Route path="settings/members" element={<Members />} />
+              </Route>
+            </Routes>
+          </BrowserRouter>
+        </Auth>
+      </ConfigProvider>
       <ReactQueryDevtools />
     </QueryClientProvider>
   )
