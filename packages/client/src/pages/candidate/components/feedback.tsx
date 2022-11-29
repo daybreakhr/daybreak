303f04b0f02f23f66cf34ca9f76e4b5ceabdce00
Apple <apple@192.168.1.2>
2022-11-29 14:36:57 +0530

    support for deleting feedback

--- packages/client/src/pages/candidate/components/feedback.tsx
+++ packages/client/src/pages/candidate/components/feedback.tsx
@@ -2,14 +2,16 @@ import { useState } from 'react'
 import dayjs from 'dayjs'
 import { useParams } from 'react-router-dom'
 import { AiOutlinePlus } from 'react-icons/ai'
-import { useQuery } from '@tanstack/react-query'
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
 import relativeTime from 'dayjs/plugin/relativeTime'
-import { Avatar, Button, Empty, Rate, Skeleton } from 'antd'
+import { Avatar, Button, Empty, Popconfirm, Rate, Skeleton } from 'antd'
 
 import { Show, Switch } from 'ui-kit'
 import FeedbackForm from './feedback-form'
-import { fetchFeedbacks } from '../queries'
+import { fetchFeedbacks, deleteFeedback } from '../queries'
 import { range } from 'lodash'
+import { DeleteOutlined } from '@ant-design/icons'
+import useAuth from 'hooks/use-auth'
 
 dayjs.extend(relativeTime)
 
@@ -17,9 +19,29 @@ export default function Feedback() {
   const { candidateId = '' } = useParams()
   const [feedbackModal, setFeedbackModal] = useState(false)
 
+  const queryClient = useQueryClient()
+  const { user } = useAuth()
+
   const { data, isLoading } = useQuery(['feedbacks', candidateId], () =>
     fetchFeedbacks(candidateId),
   )
+
+  const { mutateAsync: confirmDelete, isLoading: isDeletingFeedback } =
+    useMutation(deleteFeedback, {
+      onSuccess: () => {
+        queryClient.invalidateQueries(['feedbacks', candidateId])
+      },
+    })
+
+  function canDelete(createdBy: string) {
+    return user?.role === 'admin' || createdBy === user?.uid
+  }
+
+  function onConfirm(candidateId: string, id: string) {
+    return new Promise((resolve) => {
+      confirmDelete({ candidateId, id }).then(() => resolve(null))
+    })
+  }
 
   return (
     <div className="p-4 text-gray-800 bg-white shadow-md rounded-b-md">
@@ -62,34 +84,53 @@ export default function Feedback() {
         <Switch.Match when={data}>
           {(data) => (
             <div className="space-y-6">
-              {data.map(({ id, User, title, notes, score, createdAt }) => (
-                <div key={id} className="flex items-start space-x-4">
-                  <Avatar
-                    className="flex-none"
-                    size="large"
-                    src={User.photoURL}
-                  >
-                    {User.displayName?.charAt(0)}
-                  </Avatar>
-                  <div className="flex-1">
-                    <p className="mb-2 font-medium">{User.displayName}</p>
+              {data.map(
+                ({ id, User, title, notes, score, createdAt, createdBy }) => (
+                  <div key={id} className="flex items-start space-x-4">
+                    <Avatar
+                      className="flex-none"
+                      size="large"
+                      src={User.photoURL}
+                    >
+                      {User.displayName?.charAt(0)}
+                    </Avatar>
+                    <div className="flex-1">
+                      <div className="flex">
+                        <p className="flex-1 mb-2 font-medium">
+                          {User.displayName}
+                        </p>
+                        <Show when={canDelete(createdBy)}>
+                          <Popconfirm
+                            title="Are you sure to delete this feedback?"
+                            onConfirm={() => onConfirm(candidateId, id)}
+                            okText="Delete"
+                            cancelText="Cancel"
+                            disabled={isDeletingFeedback}
+                          >
+                            <Button
+                              danger
+                              type="text"
+                              icon={<DeleteOutlined className="text-xl" />}
+                            />
+                          </Popconfirm>
+                        </Show>
+                      </div>
+                      <div className="flex items-center justify-between mb-1">
+                        <p className="font-semibold">{title}</p>
+                        <p>
+                          <span className="mr-2 font-medium">Score</span>
+                          <Rate disabled allowHalf defaultValue={score} />
+                        </p>
+                      </div>
 
-                    <div className="flex items-center justify-between mb-1">
-                      <p className="font-semibold">{title}</p>
-
-                      <p>
-                        <span className="mr-2 font-medium">Score</span>
-                        <Rate disabled allowHalf defaultValue={score} />
+                      <p className="mb-2 whitespace-pre-line">{notes}</p>
+                      <p className="text-xs text-gray-500">
+                        {dayjs(createdAt).fromNow()}
                       </p>
                     </div>
-
-                    <p className="mb-2 whitespace-pre-line">{notes}</p>
-                    <p className="text-xs text-gray-500">
-                      {dayjs(createdAt).fromNow()}
-                    </p>
                   </div>
-                </div>
-              ))}
+                ),
+              )}
             </div>
           )}
         </Switch.Match>
