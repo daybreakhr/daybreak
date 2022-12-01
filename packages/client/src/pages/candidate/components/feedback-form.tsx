cdb9abad4c92b70d1987c348a17dc945bda8ba72
mitalisingh2022 <106293586+mitalisingh2022@users.noreply.github.com>
2022-12-01 19:31:02 +0530

    Added support to edit the feedback (#58)
    This PR adds feature for member to edit their feedback .

--- packages/client/src/pages/candidate/components/feedback-form.tsx
+++ packages/client/src/pages/candidate/components/feedback-form.tsx
@@ -1,40 +1,60 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query'
 import { Form, Input, Modal, Rate } from 'antd'
+import { useEffect } from 'react'
 import { useParams } from 'react-router-dom'
-import { createFeedback } from '../queries'
+import { Feedback } from 'types/candidate'
 
 type FeedbackFormProps = {
   visible: boolean
+  title: string
   onCancel: () => void
+  initialValues?: { id: string; title: string; notes: string; score: number }
+  mutationFunc: (args: any) => Promise<Feedback>
 }
 
-export default function FeedbackForm({ visible, onCancel }: FeedbackFormProps) {
+export default function FeedbackForm({
+  visible,
+  title,
+  onCancel,
+  initialValues,
+  mutationFunc,
+}: FeedbackFormProps) {
   const [form] = Form.useForm()
   const queryClient = useQueryClient()
   const { candidateId = '' } = useParams()
 
-  const { mutate, isLoading } = useMutation(createFeedback, {
+  const { mutate, isLoading } = useMutation(mutationFunc, {
     onSuccess: () => {
       onCancel()
       queryClient.invalidateQueries(['feedbacks', candidateId])
+      form.resetFields()
     },
   })
+
+  useEffect(() => {
+    form.setFieldsValue(initialValues)
+  }, [initialValues, form])
 
   return (
     <Modal
       visible={visible}
       onCancel={onCancel}
       onOk={() => form.submit()}
-      title="Add interview feedback"
+      title={title}
       okButtonProps={{ loading: isLoading }}
     >
       <Form
         form={form}
         layout="vertical"
-        onFinish={(values) => {
-          mutate({ candidateId, body: values })
+        onFinish={({ id, ...values }) => {
+          if (id) {
+            mutate({ id, candidateId, body: values })
+          } else {
+            mutate({ candidateId, body: values })
+          }
         }}
       >
+        <Form.Item name="id" noStyle />
         <Form.Item name="title" label="Interview Round" required>
           <Input placeholder="Enter a name for the interview round..." />
         </Form.Item>
