12790b0e99b300e284b9ee97725963c811af9304
mitalisingh2022 <106293586+mitalisingh2022@users.noreply.github.com>
2022-12-08 21:26:08 +0530

    Added tooltip for bold/underline/italic buttons (#71)
    * added tooltip for bold/underline/italic buttons
    
    * made changes in the ui-kit
    
    * deleted editor components from client/src/components
    
    Co-authored-by: Mitali Singh <mitalisingh@Mitalis-MacBook-Pro.local>

--- packages/ui-kit/src/components/editor/components.tsx
+++ packages/ui-kit/src/components/editor/components.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import clsx from 'clsx'
 import { useSlate } from 'slate-react'
+import { Tooltip } from 'antd'
 import type { RenderElementProps, RenderLeafProps } from 'slate-react'
 import {
   isBlockActive,
@@ -10,7 +11,6 @@ import {
   toggleMark,
 } from './utils'
 import { CustomText, ElementType, TextAlign } from '../../types/editor'
-
 export function Element({ attributes, children, element }: RenderElementProps) {
   switch (element.type) {
     case 'block-quote':
@@ -50,31 +50,57 @@ export function Leaf({ attributes, children, leaf }: RenderLeafProps) {
   return <span {...attributes}>{children}</span>
 }
 
+type TooltipProps = {
+  title: string
+  icons: (React.ReactNode | string)[]
+}
+
+export const TootipRenderer = ({ title, icons }: TooltipProps) => {
+  return (
+    <div className="px-2 text-center">
+      {title}
+      <div className="flex">
+        {icons.map((val, index) => (
+          <>
+            {index > 0 && '+'}
+            <div className="px-2" key={index}>
+              {val}
+            </div>
+          </>
+        ))}
+      </div>
+    </div>
+  )
+}
+
 type ButtonProps = React.DetailedHTMLProps<
   React.ButtonHTMLAttributes<HTMLButtonElement>,
   HTMLButtonElement
 > & {
   active: boolean
+  tooltip?: TooltipProps
   children: React.ReactNode
 }
 
 export const Button = React.forwardRef(
   (
-    { active, children, ...restProps }: ButtonProps,
+    { active, tooltip, children, ...restProps }: ButtonProps,
     ref?: React.Ref<HTMLButtonElement>,
   ) => {
     return (
-      <button
-        ref={ref}
-        type="button"
-        className={clsx(
-          'p-1 font-bold',
-          active ? 'text-black' : 'text-gray-500',
-        )}
-        {...restProps}
-      >
-        {children}
-      </button>
+      <Tooltip title={tooltip ? <TootipRenderer {...tooltip} /> : undefined}>
+        <button
+          ref={ref}
+          type="button"
+          className={clsx(
+            'p-1 font-bold',
+            active ? 'text-black' : 'text-gray-500',
+          )}
+          {...restProps}
+        >
+          {children}
+        </button>
+      </Tooltip>
     )
   },
 )
@@ -84,15 +110,17 @@ Button.displayName = 'Button'
 type MarkButtonProps = {
   format: keyof Omit<CustomText, 'text'>
   icon: React.ReactNode
+  tooltip?: TooltipProps
 }
 
-export function MarkButton({ format, icon }: MarkButtonProps) {
+export function MarkButton({ format, icon, tooltip }: MarkButtonProps) {
   const editor = useSlate()
 
   return (
     <Button
       active={isMarkActive(editor, format)}
       onClick={() => toggleMark(editor, format)}
+      tooltip={tooltip}
     >
       {icon}
     </Button>
