a810529ae2e38594c510115308365de17ba44ad7
Himanshu Nagrath <33781906+hnagrath09@users.noreply.github.com>
2023-01-11 21:48:43 +0530

    Create calendar module with get and post requests

--- packages/server/src/app.module.ts
+++ packages/server/src/app.module.ts
@@ -19,6 +19,7 @@ import { InvitesModule } from './invites/invites.module'
 import { TemplatesModule } from './templates/templates.module'
 import { NotificationModule } from './notification/notification.module'
 import { AffindaModule } from './affinda/affinda.module'
+import { CalendarModule } from './calendar/calendar.module'
 
 @Module({
   imports: [
@@ -52,6 +53,7 @@ import { AffindaModule } from './affinda/affinda.module'
     TemplatesModule,
     NotificationModule,
     AffindaModule,
+    CalendarModule,
   ],
   providers: [PrismaService, { provide: APP_GUARD, useClass: RolesGuard }],
 })
---
+++ packages/server/src/calendar/calendar.dto.ts
@@ -0,0 +1,49 @@
+import { ApiProperty } from '@nestjs/swagger'
+import { IsArray, IsEmail, IsString } from 'class-validator'
+
+export class CreateCalendarDto {
+  @ApiProperty({
+    example:
+      'NjVnamFwMzFjZ3NqMmJiMjYxaW02YjlrYzhxbTZiOW9jY282YWI5aWNoaGphb2hpY2hnajRjOXBjNF8yMDIzMDEyNVQxNjMwMDBaIGhpbS5uYWdyYXRoQG0',
+    description: 'EventID from google calendar',
+  })
+  @IsString()
+  eventId: string
+
+  @ApiProperty({
+    example: 'Coding round (React Project)',
+    description: 'Title of the calendar event',
+  })
+  @IsString()
+  title: string
+
+  @ApiProperty({
+    example: '2022-08-28T09:00:00-07:00',
+    description: 'start timestamp for the calendar event',
+  })
+  @IsString()
+  startTime: string
+
+  @ApiProperty({
+    example: '2022-08-28T17:00:00-07:00',
+    description: 'end timestamp for the calendar event',
+  })
+  @IsString()
+  endTime: string
+
+  @ApiProperty({
+    example: ['himanshu@daybreakhire.com', 'raj@daybreakhire.com'],
+    description: 'Email addresses of all the invited members',
+  })
+  @IsArray()
+  @IsEmail({}, { each: true })
+  attendees: string[]
+}
+
+export class CalendarDto extends CreateCalendarDto {
+  @ApiProperty({
+    example: '632af2f17685417736bc0606',
+    description: 'id from our database',
+  })
+  id: string
+}
