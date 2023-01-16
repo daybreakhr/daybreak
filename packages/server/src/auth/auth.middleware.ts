4643ff9613bcaa9bfef5b15470b14012433cb0ad
Rahul <27402666+rahulmathews@users.noreply.github.com>
2023-01-16 12:55:57 +0530

    Minor Changes

--- packages/server/src/auth/auth.middleware.ts
+++ packages/server/src/auth/auth.middleware.ts
@@ -1,17 +1,29 @@
 import { Inject, Injectable, NestMiddleware } from '@nestjs/common'
 import { NextFunction } from 'express'
+import { PrismaService } from 'src/prisma.service'
 import { AuthService } from './auth.service'
 
 @Injectable()
 export class AuthMiddleware implements NestMiddleware {
-  constructor(@Inject(AuthService) private authService: AuthService) {}
+  constructor(
+    @Inject(AuthService)
+    private authService: AuthService,
+    private prismaService: PrismaService,
+  ) {}
 
   async use(req: Request, res: Response, next: NextFunction) {
     const { authorization } = req.headers as { authorization?: string }
 
     if (authorization) {
       const user = await this.authService.verifyIdToken(authorization)
-      if (user && !user.disabled) {
+
+      const member = await this.prismaService.member.findFirst({
+        where: {
+          uid: user.uid,
+        },
+      })
+
+      if (user && !member.isSuspended) {
         // @ts-ignore
         req.user = user
       }
