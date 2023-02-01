258b4e552ece6565f443b4127f833423aa84492c
kanitmann <mannkanit@gmail.com>
2023-02-01 12:01:56 +0530

    Added localhost in dev URL

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -22,7 +22,7 @@ async function bootstrap() {
             process.env.BOARDS_URL,
             /^https:\/\/daybreakhr-.*.web.app$/,
           ]
-        : ['http://localhost:3000', 'http://localhost:5173'],
+        : ['http://localhost:3000', 'http://127.0.0.1:3000', 'http://localhost:5173', 'http://127.0.0.1:5173'],
   })
 
   const config = new DocumentBuilder()
