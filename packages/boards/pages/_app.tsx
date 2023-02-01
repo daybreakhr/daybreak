16e8133c16a64bcd4507fe9cd1a60a80e2f913d8
kanitmann <mannkanit@gmail.com>
2023-02-01 08:14:12 +0530

    Included <ConfigProvider /> to add company primary color for antd

--- packages/boards/pages/_app.tsx
+++ packages/boards/pages/_app.tsx
@@ -1,4 +1,5 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ConfigProvider } from 'antd'
 import type { AppProps } from 'next/app'
 import 'styles/globals.css'
 
@@ -7,7 +8,9 @@ const queryClient = new QueryClient()
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
-      <Component {...pageProps} />
+      <ConfigProvider theme={{ token: { colorPrimary: '#9155fd' } }}>
+        <Component {...pageProps} />
+      </ConfigProvider>
     </QueryClientProvider>
   )
 }
