9481b22450054320c695ffc8d5fd6f41cb61411f
Himanshu Nagrath <33781906+hnagrath09@users.noreply.github.com>
2023-02-03 14:17:33 +0530

    Changed layout for job view page

--- packages/boards/pages/[slug]/jobs/[jobId].tsx
+++ packages/boards/pages/[slug]/jobs/[jobId].tsx
@@ -60,7 +60,7 @@ export default function JobPage({ job }: JobPageProps) {
         <link rel="icon" type="image/svg+xml" href={job.Workspace.logo ?? ''} />
       </Head>
 
-      <div className="flex flex-col w-screen h-screen">
+      <div className="flex flex-col w-screen h-screen bg-gray-100">
         <div className="flex items-center px-6 py-3 align-middle bg-white rounded shadow-md">
           <Show when={job.Workspace.logo}>
             {(logo) => (
@@ -78,7 +78,7 @@ export default function JobPage({ job }: JobPageProps) {
           <h3 className="ml-2 font-medium text-center">{job.Workspace.name}</h3>
         </div>
 
-        <div className="flex flex-col flex-1 p-4 space-y-4 bg-gray-100">
+        <div className="flex flex-col flex-1 max-w-6xl p-4 mx-auto space-y-4 overflow-hidden">
           <PageHeader
             title={job.title}
             location={job.Location.name}
@@ -97,38 +97,42 @@ export default function JobPage({ job }: JobPageProps) {
             ]}
           />
 
-          <div className="px-8 pb-4 bg-white border border-gray-200 rounded">
-            <Tabs
-              items={tabs}
-              activeKey={selectedTab}
-              className="header-tabs"
-              onChange={handleTabChange}
-            />
-            <Show when={selectedTab === 'Overview'}>
-              <p className="mb-2 text-base font-medium">Who We Are</p>
-              <p>{job.Workspace.description}</p>
+          <div className="flex flex-col flex-1 overflow-hidden bg-white border border-gray-200 rounded">
+            <div className="px-8">
+              <Tabs
+                items={tabs}
+                activeKey={selectedTab}
+                className="header-tabs"
+                onChange={handleTabChange}
+              />
+            </div>
+            <div className="flex-1 px-8 pb-4 overflow-y-auto">
+              <Show when={selectedTab === 'Overview'}>
+                <p className="mb-2 text-base font-medium">Who We Are</p>
+                <p>{job.Workspace.description}</p>
 
-              <div className="prose max-w-none">
-                <Show when={job?.description}>
-                  {(description) => (
-                    <Reader initialValue={description as Descendant[]} />
-                  )}
-                </Show>
-              </div>
+                <div className="prose max-w-none">
+                  <Show when={job?.description}>
+                    {(description) => (
+                      <Reader initialValue={description as Descendant[]} />
+                    )}
+                  </Show>
+                </div>
 
-              <div className="flex items-center justify-center pt-5">
-                <Button
-                  type="primary"
-                  onClick={() => setSelectedTab('Application')}
-                >
-                  Apply Now
-                </Button>
-              </div>
-            </Show>
+                <div className="flex items-center justify-center pt-5">
+                  <Button
+                    type="primary"
+                    onClick={() => setSelectedTab('Application')}
+                  >
+                    Apply Now
+                  </Button>
+                </div>
+              </Show>
 
-            <Show when={selectedTab === 'Application'}>
-              <ApplicationForm workspaceId={job.Workspace.id} />
-            </Show>
+              <Show when={selectedTab === 'Application'}>
+                <ApplicationForm workspaceId={job.Workspace.id} />
+              </Show>
+            </div>
           </div>
         </div>
       </div>
