9f509c6e96d9111f587bcc62687afee90b8d55a2
Himanshu Nagrath <33781906+hnagrath09@users.noreply.github.com>
2023-03-23 15:48:39 +0530

    api integration for crud operations of interview

--- packages/client/src/pages/create-pipeline/create-pipeline.tsx
+++ packages/client/src/pages/create-pipeline/create-pipeline.tsx
@@ -1,16 +1,37 @@
 import { useState } from 'react'
-import { Button } from 'antd'
 import { range } from 'lodash'
+import { Button, Skeleton } from 'antd'
 import { Link, useLocation, useParams } from 'react-router-dom'
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
 import { LeftOutlined, PlusOutlined, RightOutlined } from '@ant-design/icons'
+
+import { Show, Switch } from 'ui-kit'
 import Stage from './components/stage'
-import EditableStage from './components/editable-stage'
+import EditableStage, { InterviewData } from './components/editable-stage'
+import { createPipelineStep, fetchInterviews } from './queries'
 
 export default function CreatePipeline() {
   const { jobId = '' } = useParams()
   const { pathname } = useLocation()
-  const [newStage, setNewStage] = useState(0)
+  const [isEditable, setIsEditable] = useState(false)
   const titlePrefix = pathname.split('/')[3]
+
+  const { data, isLoading } = useQuery(['interviews', jobId], () =>
+    fetchInterviews(jobId),
+  )
+
+  const queryClient = useQueryClient()
+  const { mutate: createInterview, isLoading: isCreatingInterview } =
+    useMutation(createPipelineStep, {
+      onSuccess: () => {
+        queryClient.invalidateQueries(['interviews', jobId])
+        setIsEditable(false)
+      },
+    })
+
+  function handleCreateInterview(values: InterviewData) {
+    createInterview({ ...values, order: data?.length ?? 0, jobId })
+  }
 
   return (
     <div className="flex flex-col h-full">
@@ -25,29 +46,42 @@ export default function CreatePipeline() {
       </div>
 
       <div className="space-y-4">
-        <Stage title="Phone Screen" color="red" />
-        <Stage title="On-site Coding" color="blue" />
-        <Stage
-          title="Bug Bash"
-          description="Fix issues in an open source react project"
-          color="green"
-        />
-        <Stage title="Hiring Manager" color="indigo" />
+        <Switch>
+          <Switch.Match when={isLoading}>
+            {range(5).map((val) => (
+              <Skeleton key={val} active title paragraph={{ rows: 0 }} />
+            ))}
+          </Switch.Match>
 
-        {range(newStage).map((val) => (
-          <EditableStage
-            key={val}
-            onClose={() => setNewStage((prev) => prev - 1)}
-          />
-        ))}
+          <Switch.Match when={data}>
+            {(data) => (
+              <>
+                {data.map(({ id, title }) => (
+                  <Stage id={id} key={id} title={title} color="red" />
+                ))}
 
-        <Button
-          type="link"
-          icon={<PlusOutlined />}
-          onClick={() => setNewStage((prev) => prev + 1)}
-        >
-          Add New Pipeline Stage
-        </Button>
+                <Show
+                  when={!isEditable}
+                  fallback={
+                    <EditableStage
+                      onSave={handleCreateInterview}
+                      isUpdating={isCreatingInterview}
+                      onClose={() => setIsEditable(false)}
+                    />
+                  }
+                >
+                  <Button
+                    type="link"
+                    icon={<PlusOutlined />}
+                    onClick={() => setIsEditable(true)}
+                  >
+                    Add New Pipeline Stage
+                  </Button>
+                </Show>
+              </>
+            )}
+          </Switch.Match>
+        </Switch>
       </div>
 
       <div className="flex-1" />
