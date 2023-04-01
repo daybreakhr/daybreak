91565a1b7a218ba2368c026a070b2a79f16d510c
hnagrath09 <him.nagrath@gmail.com>
2023-04-01 14:49:56 +0530

    added util to format number

--- packages/ui-kit/src/index.ts
+++ packages/ui-kit/src/index.ts
@@ -4,4 +4,5 @@ export { default as Switch } from './components/switch-match'
 export * from './components/remirror-editor'
 
 // utils
+export { default as formatNumber } from './utils/format-number'
 export { default as storage } from './utils/storage'
---
+++ packages/ui-kit/src/utils/format-number.ts
@@ -0,0 +1,18 @@
+/**
+ * A utility function to format number and return a string with desired styles.
+ * This function uses JavaScript's inbuilt Object Intl.NumberFormat to format the number.
+ * For more details, please refer to the following link: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/NumberFormat/NumberFormat
+ *
+ * @param {number} number - The number to be formatted.
+ * @param {string} [style] - The style to be applied to the number.
+ * @returns string with applied style. Example: "2,386,867"
+ */
+export default function formatNumber(
+  value: number,
+  options?: Intl.NumberFormatOptions,
+): string {
+  return new Intl.NumberFormat('en-US', {
+    style: 'decimal',
+    ...options,
+  }).format(value)
+}
