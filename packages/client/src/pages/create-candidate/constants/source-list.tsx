008d7a5b2d5ce07c4a970a2dabbbd1a071c9c836
Himanshu Nagrath <33781906+hnagrath09@users.noreply.github.com>
2023-05-16 10:27:23 +0530

    remove portal and referral options

--- packages/client/src/pages/create-candidate/constants/source-list.tsx
+++ packages/client/src/pages/create-candidate/constants/source-list.tsx
@@ -1,8 +1,6 @@
 import { CandidateSource } from '@prisma/client'
 
 export const candidateSources = [
-  { label: 'Portal', value: CandidateSource.jobBoard },
-  { label: 'Referral', value: CandidateSource.referral },
   { label: 'LinkedIn', value: CandidateSource.linkedIn },
   { label: 'Instahyre', value: CandidateSource.instahyre },
   { label: 'IIM Jobs', value: CandidateSource.iimjobs },
