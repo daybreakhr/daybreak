a52df9caa660cf6034ed1eb12a6fbbc96b9e023c
Himanshu Nagrath <33781906+hnagrath09@users.noreply.github.com>
2023-05-31 16:36:45 +0530

    Added Radio toggle for table and kanban view

--- packages/client/src/pages/job-pipeline/job-pipeline.tsx
+++ packages/client/src/pages/job-pipeline/job-pipeline.tsx
@@ -1,7 +1,9 @@
-import { Spin } from 'antd'
+import { Radio, Spin } from 'antd'
 import { groupBy } from 'lodash'
 import { useParams } from 'react-router-dom'
+import { MdViewKanban } from 'react-icons/md'
 import { useQueries } from '@tanstack/react-query'
+import { UnorderedListOutlined } from '@ant-design/icons'
 import { Switch } from 'ui-kit'
 
 import { getPipelineStages } from 'utils/utils'
@@ -37,9 +39,23 @@ export default function JobPipeline() {
 
   const groupByStatus = groupBy(candidates, (candidate) => candidate.status)
 
+  const viewType = [
+    { label: <UnorderedListOutlined />, value: 'table' },
+    {
+      label: (
+        <span className="text-lg anticon">
+          <MdViewKanban />
+        </span>
+      ),
+      value: 'kanban',
+    },
+  ]
+
   return (
     <div className="flex flex-col flex-1 pt-4 overflow-hidden">
-      <div className="flex items-center justify-end px-6 mb-4 space-x-4">
+      <div className="flex items-center justify-between px-6 mb-4 space-x-4">
+        <Radio.Group value="kanban" options={viewType} optionType="button" />
+
         <FilterPipeline
           interviews={interviews}
           filteredStages={filteredStages}
