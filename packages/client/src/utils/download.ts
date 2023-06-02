741db1c9e74ab4c0c66d3c4fce7ddc2338e5a140
mitalisingh2022 <106293586+mitalisingh2022@users.noreply.github.com>
2023-06-02 10:45:39 +0530

    Implement resume download and zoom-in/out functionality (#159)
    This PR adds two buttons on candidate resume section.
    1- download file
    2-zoom-in/zoom-out button

---
+++ packages/client/src/utils/download.ts
@@ -0,0 +1,23 @@
+/*
+ * @function
+ * Use HTML5 anchor tag's ability to download files
+ * by creating a virtual anchor tag element and simulate
+ * a click event to trigger the download
+ *
+ * @param {string} url - URL to download from. The response of this URL should be a file.
+ * @param {string} [filename] - Optional. Filename of the file to be downloaded. If no
+ * filename is specified, the filename given by backend is used.
+ */
+export const downloadFile = (url: string, filename?: string) => {
+  if (!url) return
+
+  const a = document.createElement('a')
+  document.body.appendChild(a)
+  a.style.display = 'none'
+  a.href = url
+  if (filename) {
+    a.download = filename
+  }
+  a.click()
+  a.remove()
+}
