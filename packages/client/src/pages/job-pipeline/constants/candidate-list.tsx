7fb6440810031c48bdb56cbe7cb5dc1be0008182
kanitmann <mannkanit@gmail.com>
2023-06-06 17:13:20 +0530

    Added candidate-source as a separate column

--- packages/client/src/pages/job-pipeline/constants/candidate-list.tsx
+++ packages/client/src/pages/job-pipeline/constants/candidate-list.tsx
@@ -1,14 +1,33 @@
 import dayjs from 'dayjs'
 import { Tag } from 'antd'
 import { capitalize } from 'lodash'
-import type { Candidate } from '@prisma/client'
+import type { Candidate, CandidateSource } from '@prisma/client'
 import { createColumnHelper } from '@tanstack/react-table'
 
 import { Show } from 'ui-kit'
+import { candidateSources, getSourceTagColor } from '../constants/icons'
 
 const columnHelper = createColumnHelper<Candidate>()
 
 export const candidateListColumns = [
+  columnHelper.accessor('source', {
+    header: 'Source',
+    cell: ({ row }) => {
+      const source = (row.original as any).source as CandidateSource
+      const sourceLabel =
+        candidateSources[source as CandidateSource] || 'Unknown'
+      const tagColor = getSourceTagColor(source as CandidateSource)
+      return (
+        <td>
+          <span className="flex">
+            <Tag className="py-1 border-none" color={tagColor}>
+              {sourceLabel}
+            </Tag>
+          </span>
+        </td>
+      )
+    },
+  }),
   columnHelper.accessor('firstName', {
     header: 'Candidate Name',
     cell: ({ row }) => {
