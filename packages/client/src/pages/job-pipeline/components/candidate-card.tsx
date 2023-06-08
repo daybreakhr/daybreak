aad4be9cd4a455817cee6d904ab3f9cbb1c68004
kanitmann <mannkanit@gmail.com>
2023-06-08 15:46:40 +0530

    Updated wrt changes in icons.tsx

--- packages/client/src/pages/job-pipeline/components/candidate-card.tsx
+++ packages/client/src/pages/job-pipeline/components/candidate-card.tsx
@@ -4,13 +4,13 @@ import { CandidateSource } from '@prisma/client'
 import { Show } from 'ui-kit'
 import { FaAward } from 'react-icons/fa'
 import { HiOfficeBuilding } from 'react-icons/hi'
-import { candidateSources, getSourceTagColor } from '../constants/icons'
+import { candidateSources } from '../constants/icons'
 
 type CandidateCardProps = {
   name: string
   createdAt: Date
   isChecked: boolean
-  source: string
+  source: CandidateSource
   currentCompany: string | null
   onCandidateSelect: () => void
   totalYearsOfExperience: number | null
@@ -25,16 +25,15 @@ export default function CandidateCard({
   onCandidateSelect,
   totalYearsOfExperience,
 }: CandidateCardProps) {
-  const sourceLabel = candidateSources[source as CandidateSource] || 'Unknown'
-  const tagColor = getSourceTagColor(source as CandidateSource)
+  const { color, icon } = candidateSources(source)
 
   return (
     <div className="w-full p-4 bg-white rounded-md shadow-md hover:bg-gray-50 group">
       <div className="relative flex items-center mb-4 space-x-2">
         <Checkbox checked={isChecked} onChange={onCandidateSelect} />
 
-        <Tag className="p-1 border-none" color={tagColor}>
-          {sourceLabel}
+        <Tag className="p-1 border-none" color={color}>
+          {icon}
         </Tag>
 
         <p className="font-semibold">{name}</p>
@@ -48,12 +47,12 @@ export default function CandidateCard({
 
       <div className="flex items-center space-x-4">
         <div className="flex items-center space-x-2 text-gray-500">
-          <HiOfficeBuilding className="w-4 h-4 " />
+          <HiOfficeBuilding className="w-4 h-4" />
           <div>{currentCompany ?? 'N/A'}</div>
         </div>
 
         <div className="flex items-center space-x-2 text-gray-500">
-          <FaAward className="w-4 h-4 " />
+          <FaAward className="w-4 h-4" />
           <p>
             <Show when={totalYearsOfExperience} fallback="N/A">
               {(value) => `${value} years`}
