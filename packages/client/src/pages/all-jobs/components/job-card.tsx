a33665b2e057bf6e3abd2cac85b6a189ae47398f
Kanit Mann <kanitmann90@gmail.com>
2023-07-04 12:48:51 +0530

    Mark job as favorite from the all jobs page (#174)
    This PR adds feature to mark job as favorite from the all jobs page

--- packages/client/src/pages/all-jobs/components/job-card.tsx
+++ packages/client/src/pages/all-jobs/components/job-card.tsx
@@ -1,22 +1,83 @@
+import type { MouseEvent } from 'react'
+import { useEffect, useState } from 'react'
 import dayjs from 'dayjs'
 import { Avatar } from 'antd'
 import { Link } from 'react-router-dom'
 import { capitalize, words } from 'lodash'
 import { RxDotFilled } from 'react-icons/rx'
+import { HiOutlineStar, HiStar } from 'react-icons/hi'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
 
-import { Job } from 'types/job'
-import { MemberWithUserInfo } from 'types/member'
 import { Show, Switch } from 'ui-kit'
+import { Job } from 'types/job'
+import useAuth from 'hooks/use-auth'
 import formatNumber from 'utils/format-number'
+import { MemberWithUserInfo } from 'types/member'
+import { updateJobById } from 'pages/create-job/queries'
 import { ReactComponent as CandidatesIcon } from 'assets/icons/candidates.svg'
 
 type JobCardProps = {
-  job: Job
+  jobData: Job
   members: MemberWithUserInfo[]
 }
 
-export default function JobCard({ job, members }: JobCardProps) {
+export default function JobCard({ jobData, members }: JobCardProps) {
+  const jobId = jobData.id
+  const { member } = useAuth()
+
+  const [job, setJob] = useState(jobData)
+
+  useEffect(() => {
+    setJob(jobData)
+  }, [jobData])
+
   const recruiter = members.find(({ uid }) => uid === job.createdBy)
+  const isJobStarred = job?.favorites.includes(member?.uid ?? '')
+
+  const queryClient = useQueryClient()
+  const { mutate } = useMutation(updateJobById, {
+    onMutate: async ({ jobId, updateJobDto }) => {
+      // Cancel any outgoing refetches
+      // (so they don't overwrite our optimistic update)
+      await queryClient.cancelQueries(['jobs'])
+      // Snapshot the previous value
+      const jobs = queryClient.getQueryData<Job[]>(['jobs'])
+      // Optimistically update to the new value
+      queryClient.setQueryData<Job[]>(['jobs'], (old) =>
+        old?.map((job) =>
+          job.id === jobId ? { ...job, ...updateJobDto } : job,
+        ),
+      )
+      // Return a context object with the snapshotted value
+      return { jobs }
+    },
+    // If the mutation fails,
+    // use the context returned from onMutate to roll back
+    onError: (_, __, context) => {
+      if (context) {
+        queryClient.setQueryData(['jobs'], context.jobs)
+      }
+    },
+    // Always refetch after error or success:
+    onSettled: () => queryClient.invalidateQueries(['jobs']),
+    onSuccess: () => {
+      queryClient.invalidateQueries(['favorite-jobs'])
+    },
+  })
+
+  function handleStarChange(event: MouseEvent<HTMLButtonElement>) {
+    event.preventDefault()
+
+    if (job && member) {
+      let favorites = job.favorites
+      if (isJobStarred) {
+        favorites = job.favorites.filter((uid) => uid !== member.uid)
+      } else {
+        favorites = [...job.favorites, member.uid]
+      }
+      mutate({ jobId, updateJobDto: { favorites } })
+    }
+  }
 
   return (
     <Link to={`${job.id}`} className="p-4 bg-white rounded-md shadow">
@@ -38,6 +99,21 @@ export default function JobCard({ job, members }: JobCardProps) {
 
         <div className="flex-1" />
 
+        <div>
+          <button
+            onClick={handleStarChange}
+            className="p-1 text-lg bg-transparent border rounded-md"
+          >
+            <Show
+              when={isJobStarred}
+              fallback={
+                <HiOutlineStar strokeWidth="1" className="text-gray-500" />
+              }
+            >
+              <HiStar className="text-yellow-500" strokeWidth={1} />
+            </Show>
+          </button>
+        </div>
         <span className="text-xs text-gray-600">
           {dayjs(job.createdAt).fromNow()}
         </span>
