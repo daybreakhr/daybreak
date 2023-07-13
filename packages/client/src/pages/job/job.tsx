2c31a8e06f19b5b47657c259bfcf224377f42d3b
Kanit Mann <kanitmann90@gmail.com>
2023-07-13 21:42:14 +0530

    Added empty state for Pipeline Page (#177)
    This PR adds an state for Job Pipeline

--- packages/client/src/pages/job/job.tsx
+++ packages/client/src/pages/job/job.tsx
@@ -3,8 +3,8 @@ import { groupBy, orderBy } from 'lodash'
 import { matchSorter } from 'match-sorter'
 import { useParams } from 'react-router-dom'
 import { Candidate, CandidateStatus } from '@prisma/client'
-import { Button, Dropdown, Input, MenuProps, Spin } from 'antd'
 import { DownOutlined, SearchOutlined } from '@ant-design/icons'
+import { Button, Dropdown, Empty, Input, MenuProps, Spin } from 'antd'
 import { useMutation, useQueries, useQueryClient } from '@tanstack/react-query'
 
 import { Show, Switch } from 'ui-kit'
@@ -14,20 +14,24 @@ import RejectModal from 'components/reject-modal'
 import useLocalStorage from 'hooks/use-local-storage'
 import { getPipelineStages, getLastDate } from 'utils/utils'
 import { fetchInterviews } from 'pages/create-pipeline/queries'
-import { ReactComponent as RejectCandidateIcon } from 'assets/icons/reject-candidate.svg'
+import { ReactComponent as TieImage } from 'assets/icons/tie-image.svg'
+import { ReactComponent as UserArrowDown } from 'assets/icons/user-arrow-down.svg'
 import { ReactComponent as MoveCandidateIcon } from 'assets/icons/move-candidate.svg'
+import { ReactComponent as RejectCandidateIcon } from 'assets/icons/reject-candidate.svg'
 
 import JobHeader from './components/job-header'
 import StatusList from './components/status-list'
 import CandidateList from './components/candidate-list'
 import FilterPipeline from './components/filter-pipeline'
-import { bulkUpdateCandidate, fetchCandidatesByJob } from './queries'
+import { bulkUpdateCandidate, fetchCandidatesByJob, fetchJob } from './queries'
+import CreateCandidate from './components/create-candidate'
 
 export default function JobPipeline() {
   const { jobId = '' } = useParams()
   const [input, setInput] = useState('')
   const [isRejectModalOpen, setIsRejectModalOpen] = useState(false)
   const [selectedSources, setSelectedSources] = useState<string[]>([])
+  const [createCandiateDrawer, setCreateCandidateDrawer] = useState(false)
   const [viewState, setViewState] = useState<'kanban' | 'table'>('kanban')
   const [selectedCandidates, setSelectedCandidates] = useState<string[]>([])
   const [filteredStages, setFilteredStages] = useLocalStorage<string[]>(
@@ -40,6 +44,7 @@ export default function JobPipeline() {
   const [
     { data: candidates = [], isLoading: isCandidatesLoading },
     { data: interviews = [], isLoading: isInterviewsLoading },
+    { data: job },
   ] = useQueries({
     queries: [
       {
@@ -49,6 +54,10 @@ export default function JobPipeline() {
       {
         queryKey: ['interviews', jobId],
         queryFn: () => fetchInterviews(jobId),
+      },
+      {
+        queryKey: ['job', jobId],
+        queryFn: () => fetchJob(jobId),
       },
     ],
   })
@@ -143,40 +152,50 @@ export default function JobPipeline() {
     <div className="flex flex-col h-full overflow-hidden">
       <JobHeader candidates={candidates} />
       <div className="flex items-center px-6 mb-4 space-x-4">
-        <ToggleView viewType={viewState} onChange={setViewState} />
-        <Show when={selectedCandidates.length > 0}>
-          <Button
-            danger
-            disabled={selectedCandidates.length === 0}
-            onClick={() => setIsRejectModalOpen(true)}
-            icon={<RejectCandidateIcon className="anticon" />}
-          >
-            Reject
-          </Button>
-          <Dropdown menu={{ items, onClick: handleBulkStatusChange }}>
-            <Button icon={<MoveCandidateIcon className="anticon" />}>
-              Move to
-              <DownOutlined />
+        <Show
+          when={
+            !isInterviewsLoading &&
+            !isCandidatesLoading &&
+            candidates?.length !== 0
+          }
+        >
+          <ToggleView viewType={viewState} onChange={setViewState} />
+          <Show when={selectedCandidates.length > 0}>
+            <Button
+              danger
+              disabled={selectedCandidates.length === 0}
+              onClick={() => setIsRejectModalOpen(true)}
+              icon={<RejectCandidateIcon className="anticon" />}
+            >
+              Reject
             </Button>
-          </Dropdown>
+            <Dropdown menu={{ items, onClick: handleBulkStatusChange }}>
+              <Button icon={<MoveCandidateIcon className="anticon" />}>
+                Move to
+                <DownOutlined />
+              </Button>
+            </Dropdown>
+          </Show>
+
+          <div className="flex-1" />
+          <Input
+            value={input}
+            placeholder="Search..."
+            style={{ width: '12rem' }}
+            suffix={<SearchOutlined />}
+            onChange={(e) => setInput(e.target.value)}
+          />
+
+          <FilterPipeline
+            interviews={interviews}
+            filteredStages={filteredStages}
+            selectedSources={selectedSources}
+            setFilteredStages={setFilteredStages}
+            setSelectedSources={setSelectedSources}
+            selectedDateFilter={selectedDateFilter}
+            setSelectedDateFilter={setSelectedDateFilter}
+          />
         </Show>
-        <div className="flex-1" />
-        <Input
-          value={input}
-          placeholder="Search..."
-          style={{ width: '12rem' }}
-          suffix={<SearchOutlined />}
-          onChange={(e) => setInput(e.target.value)}
-        />
-        <FilterPipeline
-          interviews={interviews}
-          filteredStages={filteredStages}
-          selectedSources={selectedSources}
-          setFilteredStages={setFilteredStages}
-          setSelectedSources={setSelectedSources}
-          selectedDateFilter={selectedDateFilter}
-          setSelectedDateFilter={setSelectedDateFilter}
-        />
       </div>
 
       <Switch>
@@ -184,6 +203,29 @@ export default function JobPipeline() {
           <div className="flex items-center justify-center flex-1">
             <Spin tip="Fetching Candidates..." />
           </div>
+        </Switch.Match>
+
+        <Switch.Match when={candidates?.length === 0}>
+          <Empty
+            image={<TieImage />}
+            imageStyle={{ height: '20rem' }}
+            description={
+              <span className="text-base font-medium">
+                Welcome to the Job Pipeline. <br />
+                You have not imported/added any candidates yet.
+              </span>
+            }
+          >
+            <div className="flex justify-center">
+              <Button
+                type="primary"
+                icon={<UserArrowDown className="anticon" />}
+                onClick={() => setCreateCandidateDrawer(true)}
+              >
+                Add Candidate
+              </Button>
+            </div>
+          </Empty>
         </Switch.Match>
 
         <Switch.Match when={interviews}>
@@ -222,6 +264,11 @@ export default function JobPipeline() {
       />
 
       <CandidatePage />
+      <CreateCandidate
+        title={job?.title}
+        isOpen={createCandiateDrawer}
+        onClose={() => setCreateCandidateDrawer(false)}
+      />
     </div>
   )
 }
