c979efd12caeb16c1eed816876243027cbacfab3
Kanit Mann <kanitmann90@gmail.com>
2023-07-15 20:12:00 +0530

    Added banner and padding for mobile view. (#178)
    Co-authored-by: kanitmann <mannkanit@gmail.com>

--- packages/client/src/pages/privacy-policy/privacy-policy.tsx
+++ packages/client/src/pages/privacy-policy/privacy-policy.tsx
@@ -1,194 +1,207 @@
 export default function PrivacyPolicy() {
   return (
-    <div className="max-w-3xl py-12 mx-auto prose">
-      <img
-        src="assets/logo_large.svg"
-        alt="Daybreak Logo"
-        className="w-64 mx-auto mb-8"
-      />
-      <h2 className="mx-auto text-gray-500 w-fit">Privacy Policy</h2>
-      <p className="mx-auto font-medium w-fit">Last Updated on: 29/06/2023</p>
-      <p>
-        Daybreak Technologies Private Limited is committed to protecting your
-        privacy. We have created this Privacy and Data Protection Policy to
-        describe how Daybreak collects, uses, and shares your personal
-        information.
-      </p>
-      <p>
-        This policy only applies to the use of your Personal Data by us. Please
-        note:
-      </p>
-
-      <ol>
-        <li>
-          You agree to the terms and conditions of this Policy if you choose to
-          use our products and/or services;
-        </li>
-        <li>
-          This Policy does not apply to any products, services, websites, or
-          content that are offered by third parties or have their own privacy
-          policy or notice;
-        </li>
-      </ol>
-
-      <h2 className="text-primary-500/60">Personal Information we collect </h2>
-      <p>
-        We require to collect, access, and store your personal information
-        during the period of account creation and thereafter during your usage
-        of Services. This includes your name, company, title, country, e-mail
-        address information pertaining to you as well as information pertaining
-        to third- persons/individual whose resumes will be uploaded for availing
-        the Services. We retain your information until your account remains
-        active with our application.
-      </p>
-      <p>
-        Daybreak has developed a functionality that allows its customers to
-        connect their Google Calendar and Gmail Mailbox with our product.
-        Connecting your Google Calendar and Gmail mailbox to your Daybreak
-        account allows us to associate your account with your information on
-        Google, to see your personal information, to view your email address and
-        access your emails. The connection will further allow you to respond to
-        your emails and to schedule interviews by creating events in you Google
-        Calendar directly from our product. We do not store or share any of your
-        personal information on your Google Calendar and Gmail Mailbox. You may
-        choose not to connect your Google Calendar and Gmail Mailbox with
-        Daybreak.
-      </p>
-      <p>
-        Daybreak app complies with the{' '}
-        <a
-          href="https://developers.google.com/terms/api-services-user-data-policy"
-          className="text-primary-500 hover:underline"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Google API Services User Data Policy
-        </a>
-        , including the Limited Use requirements.
-      </p>
-      <p>
-        Use and transfer to any other app of information received from Google
-        APIs will adhere to Google API Services User Data Policy, including the
-        Limited Use requirements
-      </p>
-      <p>
-        Daybreak logs and stores session usage of our product to be able to
-        debug any issues.
-      </p>
-      <p>
-        It is necessary and mandatory for us to be able to collect/store this
-        information for you to be able to use our Services and its basic
-        features. You have the right, at any time, to choose not to provide us
-        with any of this information and to ask us to delete the same, if
-        required. However, doing so may result in your subsequent deletion of
-        your account and the inability to continue using any part of Service.
-      </p>
-
-      <h2 className="text-primary-500/60">Usage of Information</h2>
-      <p>Information collected may be used for following general purposes:</p>
-      <ol>
-        <li>
-          to analyze information provided by you regarding job postings and
-          preferences, and to screen resumes and arrange our database to better
-          suit your interests;
-        </li>
-        <li>
-          to perform data analysis and statistical analysis through our tools to
-          improve the content, and to improve our Services;
-        </li>
-        <li>
-          to notify you about any changes or modifications to our Site, or terms
-          and conditions;
-        </li>
-        <li>
-          to respond quickly and efficiently to any queries, feedback, or
-          issues;
-        </li>
-        <li>
-          to engage with candidates by sending emails and scheduling interviews
-          directly from our product.
-        </li>
-      </ol>
-
-      <h2 className="text-primary-500/60">Information Storage and Security</h2>
-      <p>
-        All the information that we collect from you during your usage of
-        Services is stored on Amazon Web Services (“AWS”) servers. We access the
-        information on the cloud. AWS uses globally accepted standards for
-        ensuring and maintaining the protection, security, confidentiality, and
-        privacy of all data on its servers.
-      </p>
-
-      <h2 className="text-primary-500/60">Sharing of Information</h2>
-      <p>
-        We do not sell or otherwise share any information about you or provided
-        by you, except as described in this Policy. We may share your personal
-        information to our affiliates and third parties to help us provide and
-        improve our Services. Access to only as much information is made
-        available as is reasonably necessary or required by law. These third
-        parties are not authorized by us to use or disclose the information
-        except as necessary to perform services on our behalf.
-      </p>
-      <p>
-        In case of restructuring, reorganization, dissolution, sale, merger with
-        or being acquired by any other entity, we reserve the right to transfer
-        or share any information we have about you with such entity and the use
-        of the information thus shared shall be governed by the Privacy Policy
-        of that entity.
-      </p>
-      <p>
-        Unless otherwise provided in this Privacy Policy, we shall not share any
-        of your information with any third party except for when required in
-        relation to prevention of illegal activities or suspected fraud or when
-        required by law to do so. We also reserve the right to disclose or share
-        user information in special cases, for example, when such disclosure is
-        necessary to identify, contact or bring legal action against someone who
-        may be infringing (or threatening to infringe) or who may otherwise be
-        causing injury to or interference with (either intentionally or
-        unintentionally), our title, rights, interests or property, or those of
-        other Site users, or of anyone else who could be harmed by such
-        activities.
-      </p>
-
-      <h2 className="text-primary-500/60">Your Privacy Rights</h2>
-      <p>In accordance with applicable laws, you may have the right to:</p>
-      <ol>
-        <li>
-          Access personal information about you consistent with legal
-          requirements. In addition, you may have the right in some cases to
-          receive or have your electronic personal information transferred to
-          another party.
-        </li>
-        <li>
-          Request Correction of your personal information where it is inaccurate
-          or incomplete.
-        </li>
-        <li>
-          Request Deletion of your personal information, subject to certain
-          exceptions prescribed by law.
-        </li>
-        <li>
-          Request Restriction or Object to Processing of your personal
+    <>
+      <div className="max-w-3xl pt-12 mx-auto prose">
+        <img
+          src="assets/logo_large.svg"
+          alt="Daybreak Logo"
+          className="w-64 mx-auto mb-8"
+        />
+      </div>
+      <div className="w-screen py-2 my-4 bg-purple-600">
+        <h2 className="mx-auto text-5xl font-medium text-white md:text-xl w-fit">
+          Privacy Policy
+        </h2>
+      </div>
+      <div className="max-w-3xl px-4 pb-12 mx-auto prose">
+        <p className="mx-auto font-medium w-fit">Last Updated on: 29/06/2023</p>
+        <p>
+          Daybreak Technologies Private Limited is committed to protecting your
+          privacy. We have created this Privacy and Data Protection Policy to
+          describe how Daybreak collects, uses, and shares your personal
           information.
-        </li>
-      </ol>
-      <p>
-        If you would like to exercise any of these rights, please contact us at{' '}
-        <a
-          href="mailto:himanshu@daybreakhire.com"
-          className="text-primary-500 hover:underline"
-        >
-          himanshu@daybreakhire.com
-        </a>
-        . We will process such requests in accordance with applicable laws.
-      </p>
+        </p>
+        <p>
+          This policy only applies to the use of your Personal Data by us.
+          Please note:
+        </p>
 
-      <h2 className="text-primary-500/60">Changes to Our Privacy Policy</h2>
-      <p>
-        We may revise this Privacy Policy from time to time at our sole
-        discretion. If there are any material changes to this Privacy Policy, we
-        will notify you as required by applicable law.
-      </p>
-    </div>
+        <ol>
+          <li>
+            You agree to the terms and conditions of this Policy if you choose
+            to use our products and/or services;
+          </li>
+          <li>
+            This Policy does not apply to any products, services, websites, or
+            content that are offered by third parties or have their own privacy
+            policy or notice;
+          </li>
+        </ol>
+
+        <h2 className="text-primary-500/60">
+          Personal Information we collect{' '}
+        </h2>
+        <p>
+          We require to collect, access, and store your personal information
+          during the period of account creation and thereafter during your usage
+          of Services. This includes your name, company, title, country, e-mail
+          address information pertaining to you as well as information
+          pertaining to third- persons/individual whose resumes will be uploaded
+          for availing the Services. We retain your information until your
+          account remains active with our application.
+        </p>
+        <p>
+          Daybreak has developed a functionality that allows its customers to
+          connect their Google Calendar and Gmail Mailbox with our product.
+          Connecting your Google Calendar and Gmail mailbox to your Daybreak
+          account allows us to associate your account with your information on
+          Google, to see your personal information, to view your email address
+          and access your emails. The connection will further allow you to
+          respond to your emails and to schedule interviews by creating events
+          in you Google Calendar directly from our product. We do not store or
+          share any of your personal information on your Google Calendar and
+          Gmail Mailbox. You may choose not to connect your Google Calendar and
+          Gmail Mailbox with Daybreak.
+        </p>
+        <p>
+          Daybreak app complies with the{' '}
+          <a
+            href="https://developers.google.com/terms/api-services-user-data-policy"
+            className="text-primary-500 hover:underline"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Google API Services User Data Policy
+          </a>
+          , including the Limited Use requirements.
+        </p>
+        <p>
+          Use and transfer to any other app of information received from Google
+          APIs will adhere to Google API Services User Data Policy, including
+          the Limited Use requirements
+        </p>
+        <p>
+          Daybreak logs and stores session usage of our product to be able to
+          debug any issues.
+        </p>
+        <p>
+          It is necessary and mandatory for us to be able to collect/store this
+          information for you to be able to use our Services and its basic
+          features. You have the right, at any time, to choose not to provide us
+          with any of this information and to ask us to delete the same, if
+          required. However, doing so may result in your subsequent deletion of
+          your account and the inability to continue using any part of Service.
+        </p>
+
+        <h2 className="text-primary-500/60">Usage of Information</h2>
+        <p>Information collected may be used for following general purposes:</p>
+        <ol>
+          <li>
+            to analyze information provided by you regarding job postings and
+            preferences, and to screen resumes and arrange our database to
+            better suit your interests;
+          </li>
+          <li>
+            to perform data analysis and statistical analysis through our tools
+            to improve the content, and to improve our Services;
+          </li>
+          <li>
+            to notify you about any changes or modifications to our Site, or
+            terms and conditions;
+          </li>
+          <li>
+            to respond quickly and efficiently to any queries, feedback, or
+            issues;
+          </li>
+          <li>
+            to engage with candidates by sending emails and scheduling
+            interviews directly from our product.
+          </li>
+        </ol>
+
+        <h2 className="text-primary-500/60">
+          Information Storage and Security
+        </h2>
+        <p>
+          All the information that we collect from you during your usage of
+          Services is stored on Amazon Web Services (“AWS”) servers. We access
+          the information on the cloud. AWS uses globally accepted standards for
+          ensuring and maintaining the protection, security, confidentiality,
+          and privacy of all data on its servers.
+        </p>
+
+        <h2 className="text-primary-500/60">Sharing of Information</h2>
+        <p>
+          We do not sell or otherwise share any information about you or
+          provided by you, except as described in this Policy. We may share your
+          personal information to our affiliates and third parties to help us
+          provide and improve our Services. Access to only as much information
+          is made available as is reasonably necessary or required by law. These
+          third parties are not authorized by us to use or disclose the
+          information except as necessary to perform services on our behalf.
+        </p>
+        <p>
+          In case of restructuring, reorganization, dissolution, sale, merger
+          with or being acquired by any other entity, we reserve the right to
+          transfer or share any information we have about you with such entity
+          and the use of the information thus shared shall be governed by the
+          Privacy Policy of that entity.
+        </p>
+        <p>
+          Unless otherwise provided in this Privacy Policy, we shall not share
+          any of your information with any third party except for when required
+          in relation to prevention of illegal activities or suspected fraud or
+          when required by law to do so. We also reserve the right to disclose
+          or share user information in special cases, for example, when such
+          disclosure is necessary to identify, contact or bring legal action
+          against someone who may be infringing (or threatening to infringe) or
+          who may otherwise be causing injury to or interference with (either
+          intentionally or unintentionally), our title, rights, interests or
+          property, or those of other Site users, or of anyone else who could be
+          harmed by such activities.
+        </p>
+
+        <h2 className="text-primary-500/60">Your Privacy Rights</h2>
+        <p>In accordance with applicable laws, you may have the right to:</p>
+        <ol>
+          <li>
+            Access personal information about you consistent with legal
+            requirements. In addition, you may have the right in some cases to
+            receive or have your electronic personal information transferred to
+            another party.
+          </li>
+          <li>
+            Request Correction of your personal information where it is
+            inaccurate or incomplete.
+          </li>
+          <li>
+            Request Deletion of your personal information, subject to certain
+            exceptions prescribed by law.
+          </li>
+          <li>
+            Request Restriction or Object to Processing of your personal
+            information.
+          </li>
+        </ol>
+        <p>
+          If you would like to exercise any of these rights, please contact us
+          at{' '}
+          <a
+            href="mailto:himanshu@daybreakhire.com"
+            className="text-primary-500 hover:underline"
+          >
+            himanshu@daybreakhire.com
+          </a>
+          . We will process such requests in accordance with applicable laws.
+        </p>
+
+        <h2 className="text-primary-500/60">Changes to Our Privacy Policy</h2>
+        <p>
+          We may revise this Privacy Policy from time to time at our sole
+          discretion. If there are any material changes to this Privacy Policy,
+          we will notify you as required by applicable law.
+        </p>
+      </div>
+    </>
   )
 }
